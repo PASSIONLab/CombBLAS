35aa05cb8f4e7151ef9bb9ede6df267764c649e3
alugowski <alugowski@localhost>
2010-11-03 21:49:28 +0000

    pyCombBLAS' makefile updated to properly link in graph500 generator. testscript.py works with new names

--- Python/pyCombBLAS/testscript.py
+++ Python/pyCombBLAS/testscript.py
@@ -1,11 +1,10 @@
 import pyCombBLAS as pcb
 
-g = pcb.DiGraph()
+g = pcb.pySpParMat()
+print "loading matrix from /home/alugowski/matrices/rmat_scale14.mtx";
 g.load("/home/alugowski/matrices/rmat_scale14.mtx");
 print(g.nedges())
 
 del g
-
-print "1"
 
 pcb.finalize()
