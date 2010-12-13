09e27691cdda956c32180cf6446ed5cbc518f55f
spr <spr@localhost>
2010-12-13 18:04:57 +0000

    =Modified distribCombBLAS to reflect Adam's anticipated changes to pyCombBLAS

--- Python/distribCombBLAS/DiGraph.py
+++ Python/distribCombBLAS/DiGraph.py
@@ -75,44 +75,26 @@ def Graph500Edges(n):						# NOTE: Not changed yet from SciPy version
  	return gr.EdgeV(rowcol, sc.tile(1,(N*4,)))
 
 
-#	creates a breadth-first search tree of a Graph from a starting set of vertices
-#	returns a 1D array with the parent vertex of each vertex in the tree; unreached vertices have parent == -Inf
-#        and a 1D array with the level at which each vertex was first discovered (-2 if not in the tree)
-
-def bfsTree(G, starts):
-	#old parents = -2*sc.ones(G.spmat.shape[0]).astype(int);
-	parents = spv.zeros(G.nverts()) - 2;				# constructor and -=
-	#old levels = np.copy(parents);
-	levels = parents;					# copy() (or "=") DPV->DPV (NEW) 
-	#old newverts = np.copy(starts);
-	newverts = spv.SpParVec(starts);				# need constructor (or "=") from scalar (NEW)
-	parents[newverts] = -1;						# overload __setitem__ (NEW)
-	levels[newverts] = 0;
-	#fringe = np.array([newverts]);
-	fringe = newverts;
-	level = 1;
-	#old while len(fringe) > 0:
-	while len(fringe) > 0							# 'len' overloaded for SPVs
-		#old colvec = sc.zeros((G.nverts(),));
-		colvec = spv.zeros(G.nverts());
-		# +1 to deal with 0 being a valid vertex ID
-		#old colvec[fringe] = fringe+1; 
-		colvec[fringe] = fringe+1
-		#old cand = gr.Graph._SpMV_times_max(G.spmat, colvec)
-		cand = gr.Graph._SpMV_SelMax(G.spmat, colvec);	# 
-		#old newverts = np.array(((cand.toarray().flatten() <> 0) & (parents == -2)).nonzero()).flatten();
-		tmp1 = ~(spv.bool(cand)) &  spv.bool(parents+2) );		
-		newverts = tmp1.nonzero();				
-		#old if len(newverts) > 0:
-		if spv.reduce(~(spv.bool(newverts)), +)
-			#old parents[newverts] = cand[newverts].todense().astype(int) - 1;
-			parents[newverts] = cand[newverts] - 1;
-			levels[newverts] = level;
-		level += 1;
-		fringe = newverts;
-	#old parents = parents.astype(int);
-	parents = int(parents);
-	return (parents, levels);
+#	creates a breadth-first search tree of a Graph from a starting
+#	set of vertices.  Returns a 1D array with the parent vertex of 
+#	each vertex in the tree; unreached vertices have parent == -Inf.
+#
+def bfsTree(G, start):
+	parents = pcb.DenseParVec(G.nverts()) - 1;
+	# NOTE:  values in fringe go from 1:n instead of 0:(n-1) so can
+	# distinguish vertex0 from empty element
+	fringe = pcb.SpParVec(G.nverts());
+	parents[start] = start;
+	fringe[start] = start+1;
+	while fringe.getnnz() > 0
+		#FIX:  following line needed?
+		fringe = pcb.range(fringe);	# or fringe.setNumToInd();
+		G.SpMV_SelMax_inplace(fringe);	
+		pcb.EWiseMult_inplacefirst(fringe, parents, True, -1);
+		parents[fringe-1] = 0
+		parents += fringe-1;
+	parents = int(parents);		#FIX: needed?  how done?
+	return parents;
 
 
 
