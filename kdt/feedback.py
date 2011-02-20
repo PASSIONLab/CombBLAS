295b0d424654579aa2b86c37f3bc92516b59f36a
spr <spr@localhost>
2011-02-20 18:18:32 +0000

    =cleanup of ParVec/SpParVec to use Apply/EWise/Apply/Reduce directly as much as possible and not use pyCombBLAS:all/any/max/min/Count functions if possible to avoid them;  also a number of small fixes;  also docstrings for most ParVec and SpParVec methods

--- kdt/feedback.py
+++ kdt/feedback.py
@@ -9,7 +9,6 @@ class feedback:
 	_kdt_LogFname = '.KDT_log';
 	_kdt_EmailFname = 'KDT_email';
 	
-	#FIX:  replace the following line by the alias, when the alias exists
 	#NOTE:  as of 2011Jan17, putting multiple email addresses in the _kdt_Alias
 	#	variable, whether separated by spaces, commas, or semicolons, did
 	#	not work properly.
