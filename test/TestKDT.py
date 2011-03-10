1fb791cec8dee6b4ce076286df43c0e658212154
davidalber <davidalber@localhost>
2011-03-10 14:47:29 +0000

    Add TestKDT.py, which builds a suite of all the separate packages' tests.

---
+++ test/TestKDT.py
@@ -0,0 +1,18 @@
+import unittest
+import TestDiGraph
+import TestParVec
+import TestSpParVec
+
+def runTests(verbosity = 1):
+    testSuite = suite()
+    unittest.TextTestRunner(verbosity=verbosity).run(testSuite)
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTests(TestDiGraph.suite())
+    suite.addTests(TestParVec.suite())
+    suite.addTests(TestSpParVec.suite())
+    return suite
+
+if __name__ == '__main__':
+    runTests()
