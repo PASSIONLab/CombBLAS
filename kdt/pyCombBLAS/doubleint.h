7d7c18b34ee2a271ae67a61a83f5d103356f1582
alugowski <alugowski@localhost>
2011-03-10 19:00:48 +0000

    builds with PGI compiler

--- kdt/pyCombBLAS/doubleint.h
+++ kdt/pyCombBLAS/doubleint.h
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <limits>
 
+using namespace std;
+
 struct doubleint 
 {
 	double d;
@@ -13,7 +15,7 @@ struct doubleint
 	doubleint(double v):			d(v) {}
 	doubleint(const doubleint & v):	d(v.d) {}
 	
-	bool isnan() const							{ return std::isnan(d); }
+	bool is_nan() const							{ return isnan(d); }
 	static doubleint nan()						{ return doubleint(numeric_limits<double>::quiet_NaN()); } 
 	
 	//operator unsigned long() const				{ double ret = (d < 0 ? std::ceil(d-0.5) : std::floor(d+0.5)); return static_cast<unsigned long>(ret); }
@@ -45,27 +47,27 @@ struct doubleint
 // Binary operators. The preprocessor can help save a lot of monotony.
 
 #define BINARY_OPERATOR_INTONLY(op, returntype, doubleinttype) \
-inline returntype operator op(const doubleint& lhs, const doubleint& rhs)	{ return returntype((doubleinttype)lhs op (doubleinttype)rhs ); } \
-inline returntype operator op(const int lhs, const doubleint& rhs)			{ return returntype(lhs op (doubleinttype)rhs ); } \
-inline returntype operator op(const int64_t lhs, const doubleint& rhs)		{ return returntype(lhs op (doubleinttype)rhs ); } \
-inline returntype operator op(const uint64_t lhs, const doubleint& rhs)		{ return returntype(lhs op (doubleinttype)rhs ); } \
-inline returntype operator op(const doubleint& lhs, const int rhs)			{ return returntype((doubleinttype)lhs op rhs ); } \
-inline returntype operator op(const doubleint& lhs, const int64_t rhs)		{ return returntype((doubleinttype)lhs op rhs ); } \
-inline returntype operator op(const doubleint& lhs, const uint64_t rhs)		{ return returntype((doubleinttype)lhs op rhs ); } \
+inline returntype operator op(const doubleint& lhs, const doubleint& rhs)	{ return returntype((doubleinttype)lhs.d op (doubleinttype)rhs.d ); } \
+inline returntype operator op(const int lhs, const doubleint& rhs)			{ return returntype(lhs op (doubleinttype)rhs.d ); } \
+inline returntype operator op(const int64_t lhs, const doubleint& rhs)		{ return returntype(lhs op (doubleinttype)rhs.d ); } \
+inline returntype operator op(const uint64_t lhs, const doubleint& rhs)		{ return returntype(lhs op (doubleinttype)rhs.d ); } \
+inline returntype operator op(const doubleint& lhs, const int rhs)			{ return returntype((doubleinttype)lhs.d op rhs ); } \
+inline returntype operator op(const doubleint& lhs, const int64_t rhs)		{ return returntype((doubleinttype)lhs.d op rhs ); } \
+inline returntype operator op(const doubleint& lhs, const uint64_t rhs)		{ return returntype((doubleinttype)lhs.d op rhs ); } \
 
 
 #define BINARY_OPERATOR(op, returntype, doubleinttype) \
-inline returntype operator op(const double lhs, const doubleint& rhs)		{ return returntype(lhs op (doubleinttype)rhs ); } \
-inline returntype operator op(const doubleint& lhs, const double rhs)		{ return returntype((doubleinttype)lhs op rhs ); } \
+inline returntype operator op(const double lhs, const doubleint& rhs)		{ return returntype(lhs op (doubleinttype)rhs.d ); } \
+inline returntype operator op(const doubleint& lhs, const double rhs)		{ return returntype((doubleinttype)lhs.d op rhs ); } \
 BINARY_OPERATOR_INTONLY(op, returntype, doubleinttype)
 
 
 
 #define BINARY_OPERATOR_EQ_INTONLY(op, opeq, returntype, doubleinttype) \
-inline doubleint& operator opeq(doubleint& lhs, const doubleint& rhs)		{ lhs.d = static_cast<double>(((doubleinttype)lhs) op ((doubleinttype)rhs)); return lhs; } \
-inline doubleint& operator opeq(doubleint& lhs, const int rhs)				{ lhs.d = static_cast<double>(((doubleinttype)lhs) op rhs); return lhs; } \
-inline doubleint& operator opeq(doubleint& lhs, const int64_t rhs)			{ lhs.d = static_cast<double>(((doubleinttype)lhs) op rhs); return lhs; } \
-inline doubleint& operator opeq(doubleint& lhs, const uint64_t rhs)			{ lhs.d = static_cast<double>(((doubleinttype)lhs) op rhs); return lhs; } \
+inline doubleint& operator opeq(doubleint& lhs, const doubleint& rhs)		{ lhs.d = static_cast<double>(((doubleinttype)lhs.d) op ((doubleinttype)rhs.d)); return lhs; } \
+inline doubleint& operator opeq(doubleint& lhs, const int rhs)				{ lhs.d = static_cast<double>(((doubleinttype)lhs.d) op rhs); return lhs; } \
+inline doubleint& operator opeq(doubleint& lhs, const int64_t rhs)			{ lhs.d = static_cast<double>(((doubleinttype)lhs.d) op rhs); return lhs; } \
+inline doubleint& operator opeq(doubleint& lhs, const uint64_t rhs)			{ lhs.d = static_cast<double>(((doubleinttype)lhs.d) op rhs); return lhs; } \
 
 #define BINARY_OPERATOR_EQ(op, opeq, returntype, doubleinttype) \
 inline doubleint& operator opeq(doubleint& lhs, const double rhs)			{ lhs.d opeq rhs; return lhs; } \
@@ -73,9 +75,9 @@ BINARY_OPERATOR_EQ_INTONLY(op, opeq, returntype, doubleinttype)
 
 
 #define BINARY_OPERATOR_LOGICAL(op) \
-inline bool operator op(const bool lhs, const doubleint& rhs)				{ return (lhs op (int64_t)rhs ); } \
-inline bool operator op(const doubleint& lhs, const bool rhs)				{ return ((int64_t)lhs op rhs ); } \
-inline bool operator op(const doubleint& lhs, const doubleint& rhs)			{ return ((int64_t)lhs op (int64_t)rhs ); } \
+inline bool operator op(const bool lhs, const doubleint& rhs)				{ return (lhs op (int64_t)rhs.d ); } \
+inline bool operator op(const doubleint& lhs, const bool rhs)				{ return ((int64_t)lhs.d op rhs ); } \
+inline bool operator op(const doubleint& lhs, const doubleint& rhs)			{ return ((int64_t)lhs.d op (int64_t)rhs.d ); } \
 
 
 BINARY_OPERATOR(+, doubleint, double)
