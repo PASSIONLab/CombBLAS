869abb91db7cf1362cb99a01bd459d1dcb2029ae
aydin <aydin@localhost>
2011-07-24 02:24:30 +0000

    ii uyea

--- CombBLAS/FullyDist.h
+++ CombBLAS/FullyDist.h
@@ -59,9 +59,20 @@ class FullyDist
 {};	// dummy generic template
 
 
-// ABAB: Both derived classes, whether sparse or dense, are distributed
-// to processors based on their "length", so that a conversion does not
-// need any communication between sparse and dense formats
+/** 
+  * The full distribution is actually a two-level distribution that matches the matrix distribution
+  * In this scheme, each processor row (except the last) is responsible for t = floor(n/sqrt(p)) elements. 
+  * The last processor row gets the remaining (n-floor(n/sqrt(p))*(sqrt(p)-1)) elements
+  * Within the processor row, each processor (except the last) is responsible for loc = floor(t/sqrt(p)) elements. 
+  * Example: n=103 and p=16
+  * All processors P_ij for i=0,1,2 and j=0,1,2 get floor(floor(102/4)/4) = 6 elements
+  * All processors P_i3 for i=0,1,2 get 25-6*3 = 7 elements
+  * All processors P_3j for j=0,1,2 get (102-25*3)/4 = 6 elements
+  * Processor P_33 gets 27-6*3 = 9 elements  
+  * Both derived classes, whether sparse or dense, are distributed
+  * to processors based on their "length", so that a conversion does not
+  * need any communication between sparse and dense formats
+ **/
 template <class IT, class NT>
 class FullyDist<IT, NT, typename disable_if< is_boolean<NT>::value, NT >::type >
 {
@@ -175,17 +186,6 @@ int FullyDist<IT,NT,typename disable_if< is_boolean<NT>::value, NT >::type >
 	return own_proccol;
 }
 
-
-
-// The full distribution is actually a two-level distribution that matches the matrix distribution
-// In this scheme, each processor row (except the last) is responsible for t = floor(n/sqrt(p)) elements. 
-// The last processor row gets the remaining (n-floor(n/sqrt(p))*(sqrt(p)-1)) elements
-// Within the processor row, each processor (except the last) is responsible for loc = floor(t/sqrt(p)) elements. 
-// Example: n=103 and p=16
-// All processors P_ij for i=0,1,2 and j=0,1,2 get floor(floor(102/4)/4) = 6 elements
-// All processors P_i3 for i=0,1,2 get 25-6*3 = 7 elements
-// All processors P_3j for j=0,1,2 get (102-25*3)/4 = 6 elements
-// Processor P_33 gets 27-6*3 = 9 elements  
 template <class IT, class NT>
 IT FullyDist<IT,NT,typename disable_if< is_boolean<NT>::value, NT >::type >
 ::LengthUntil() const
@@ -201,9 +201,7 @@ IT FullyDist<IT,NT,typename disable_if< is_boolean<NT>::value, NT >::type >
 
 	int proccols = commGrid->GetGridCols();
 	int my_proccol = commGrid->GetRankInProcRow();
-
 	IT n_perproc = n_thisrow / proccols;	// length on a typical processor
-
 	return ((n_perprocrow * my_procrow)+(n_perproc*my_proccol));
 }
 
@@ -223,9 +221,7 @@ IT FullyDist<IT,NT,typename disable_if< is_boolean<NT>::value, NT >::type >
 
 	int proccols = commGrid->GetGridCols();
 	int my_proccol = commGrid->GetRankInProcRow();
-
 	IT n_perproc = n_thisrow / proccols;	// length on a typical processor
-
 	return (n_perproc*my_proccol);
 }
 
@@ -245,11 +241,9 @@ IT FullyDist<IT,NT,typename disable_if< is_boolean<NT>::value, NT >::type >
 
 	int proccols = commGrid->GetGridCols();
 	IT n_perproc = n_thisrow / proccols;	// length on a typical processor
-
 	assert(k < proccols);
 	return (n_perproc*k);
 }
-
 
 template <class IT, class NT>
 IT FullyDist<IT,NT, typename disable_if< is_boolean<NT>::value, NT >::type>
