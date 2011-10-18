b8b8514b85314c302c1d348f4f1e5596dc79efa3
aydin <aydin@localhost>
2011-10-18 23:05:58 +0000

    Cool

--- CombBLAS/FullyDistSpVec.h
+++ CombBLAS/FullyDistSpVec.h
@@ -226,9 +226,8 @@ private:
 	friend FullyDistSpVec<IU,typename promote_trait<NUM,IU>::T_promote>  
 	SpMV (const SpParMat<IU,NUM,UDER> & A, const FullyDistSpVec<IU,IU> & x, bool indexisvalue);
 
-	template <typename SR, typename IU, typename NUM, typename UDER> 
-	friend FullyDistSpVec<IU,typename promote_trait<NUM,IU>::T_promote>  
-	SpMV (const SpParMat<IU,NUM,UDER> & A, const FullyDistSpVec<IU,IU> & x, bool indexisvalue, OptBuf<int32_t, typename promote_trait<NUM,IU>::T_promote > & optbuf);
+	template <typename SR, typename IVT, typename OVT, typename IU, typename NUM, typename UDER>
+	friend void SpMV (const SpParMat<IU,NUM,UDER> & A, const FullyDistSpVec<IU,IVT> & x, FullyDistSpVec<IU,OVT> & y,bool indexisvalue, OptBuf<int32_t, OVT > & optbuf);
 
 	template <typename IU, typename NU1, typename NU2>
 	friend FullyDistSpVec<IU,typename promote_trait<NU1,NU2>::T_promote> 
@@ -248,8 +247,12 @@ private:
 	template <typename IU>
 	friend void RenameVertices(DistEdgeList<IU> & DEL);
 	
-	template<typename IU>
-	friend void TransposeVector(MPI::Intracomm & World, const FullyDistSpVec<IU,IU> & x, IU & trxlocnz, IU & lenuntil, int32_t * & trxinds, IU * & trxnums, bool indexisvalue);
+	//! Helper functions for sparse matrix X sparse vector
+	template <typename SR, typename IU, typename OVT>
+	friend void MergeContributions(FullyDistSpVec<IU,OVT> & y, int * & recvcnt, int * & rdispls, int32_t * & recvindbuf, OVT * & recvnumbuf, int rowneighs);
+	
+	template<typename IU, typename NV>
+	friend void TransposeVector(MPI::Intracomm & World, const FullyDistSpVec<IU,NV> & x, int32_t & trxlocnz, IU & lenuntil, int32_t * & trxinds, NV * & trxnums, bool indexisvalue);
 };
 
 #include "FullyDistSpVec.cpp"
