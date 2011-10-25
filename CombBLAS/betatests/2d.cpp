dbc5f83b89dd92a2789db0a3c6ca40dec703a085
aydin <aydin@localhost>
2011-10-25 16:40:23 +0000

    staggered runs

--- CombBLAS/betatests/2d.cpp
+++ CombBLAS/betatests/2d.cpp
@@ -14,11 +14,16 @@ bool from_string(T & t, const string& s, std::ios_base& (*f)(std::ios_base&))
 	return !(iss >> f >> t).fail();
 }
 
-void DoAG(MPI_Comm & World, int N)
+void DoAG(MPI_Comm & World, int N, MPI_Comm & OtherDimension)
 {
 	int size, rank;
 	MPI_Comm_size(World, &size); 
 	MPI_Comm_rank(World, &rank);
+
+	int complementrank;	// rank in the complementing dimension - determines the order in which this processor will participate
+	int complementsize;
+	MPI_Comm_size(OtherDimension, &complementsize); 
+	MPI_Comm_rank(OtherDimension, &complementrank);
 
 	int * recvcnt = new int[size];
 	N = N + rank;	// add some noise
@@ -57,9 +62,33 @@ void DoAG(MPI_Comm & World, int N)
 		cout << "Average time: " << avetime << " seconds" << endl;
 		cout << "Max time: " << maxtime << " seconds" << endl;
 		cout << "Average bandwidth: " << (static_cast<double>(totrecv)*sizeof(double))/(avetime) << " bytes/sec" << endl;
+		cout << "Minimum bandwidth: " << (static_cast<double>(totrecv)*sizeof(double))/(maxtime) << " bytes/sec" << endl;
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
+	// Test the case without contention
+	for(int i=0; i< complementsize; ++i)
+	{
+		t1 = MPI_Wtime();	// globally synchronizing if MPI_WTIME_IS_GLOBAL is true
+		if(i == complementrank)		MPI_Allgatherv(data, N, MPI_DOUBLE, recvbuf, recvcnt, dpls, MPI_DOUBLE, World);
+		t2 = MPI_Wtime();
+		if(i == complementrank)  	time = t2-t1;
+	}
+	// By now, every processor has overridden "time" exactly once
+	MPI_Reduce( &time, &maxtime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+	MPI_Reduce( &time, &avetime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	avetime = avetime / static_cast<double>(nprocs);
+
+	if(grank == 0)
+	{
+		cout << "*** Runs in sequence (decoupling contention among subcommunicators) ***" << endl;
+		cout << "Total data received (per proc): " << totrecv << " doubles" << endl;
+		cout << "Average time: " << avetime << " seconds" << endl;
+		cout << "Max time: " << maxtime << " seconds" << endl;
+		cout << "Average bandwidth: " << (static_cast<double>(totrecv)*sizeof(double))/(avetime) << " bytes/sec" << endl;
+		cout << "Minimum bandwidth: " << (static_cast<double>(totrecv)*sizeof(double))/(maxtime) << " bytes/sec" << endl;
+	}
+
 	t1 = MPI_Wtime();
 	for(int i=0; i< 10; ++i)
 		MPI_Allgatherv(data, N, MPI_DOUBLE, recvbuf, recvcnt, dpls, MPI_DOUBLE, World);
@@ -77,6 +106,7 @@ void DoAG(MPI_Comm & World, int N)
                 cout << "Average time: " << avetime/10 << " seconds" << endl;
 		cout << "Max time: " << maxtime/10 << " seconds" << endl;
                 cout << "Bandwidth (average): " << (static_cast<double>(totrecv)*sizeof(double)*10.0)/(avetime) << " bytes/sec" << endl;
+                cout << "Bandwidth (minimum): " << (static_cast<double>(totrecv)*sizeof(double)*10.0)/(maxtime) << " bytes/sec" << endl;
         }
 
 	delete [] recvcnt;
@@ -86,11 +116,16 @@ void DoAG(MPI_Comm & World, int N)
 }
 
 
-void DoA2A(MPI_Comm & World, int N)
+void DoA2A(MPI_Comm & World, int N, MPI_Comm & OtherDimension)
 {
 	int size, rank;
 	MPI_Comm_size(World, &size); 
 	MPI_Comm_rank(World, &rank);
+
+	int complementrank;	// rank in the complementing dimension - determines the order in which this processor will participate
+	int complementsize;
+	MPI_Comm_size(OtherDimension, &complementsize); 
+	MPI_Comm_rank(OtherDimension, &complementrank);
 
 	int * sendcnt = new int[size];
 	int * recvcnt = new int[size];
@@ -140,6 +175,29 @@ void DoA2A(MPI_Comm & World, int N)
 	{
 		cout << "Alltoallv grid size: " << size << endl;
 		cout << "Total data received: " << totrecv << " doubles" << endl;
+		cout << "Average time: " << avetime << " seconds" << endl;
+		cout << "Max time: " << maxtime << " seconds" << endl;
+		cout << "Average bandwidth: " << (static_cast<double>(totrecv)*sizeof(double))/(avetime) << " bytes/sec" << endl;
+		cout << "Minimum bandwidth: " << (static_cast<double>(totrecv)*sizeof(double))/(maxtime) << " bytes/sec" << endl;
+	}
+
+	// Test the case without contention
+	for(int i=0; i< complementsize; ++i)
+	{
+		t1 = MPI_Wtime();	// globally synchronizing if MPI_WTIME_IS_GLOBAL is true
+		if(i == complementrank)		MPI_Alltoallv(data, sendcnt, sdispls, MPI_DOUBLE, recvbuf, recvcnt, rdispls, MPI_DOUBLE, World);
+		t2 = MPI_Wtime();
+		if(i == complementrank)  	time = t2-t1;
+	}
+	// By now, every processor has overridden "time" exactly once
+	MPI_Reduce( &time, &maxtime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+	MPI_Reduce( &time, &avetime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	avetime = avetime / static_cast<double>(nprocs);
+
+	if(grank == 0)
+	{
+		cout << "*** Runs in sequence (decoupling contention among subcommunicators) ***" << endl;
+		cout << "Total data received (per proc): " << totrecv << " doubles" << endl;
 		cout << "Average time: " << avetime << " seconds" << endl;
 		cout << "Max time: " << maxtime << " seconds" << endl;
 		cout << "Average bandwidth: " << (static_cast<double>(totrecv)*sizeof(double))/(avetime) << " bytes/sec" << endl;
@@ -200,13 +258,13 @@ int main(int argc, char* argv[])
     	MPI_Comm_split( MPI_COMM_WORLD, myprocrow, rank, &squarerowcomm );  // processes with the same color are in the same new communicator 
     	MPI_Comm_split( MPI_COMM_WORLD, myproccol, rank, &squarecolcomm );
 	if(rank == 0) cout << "*** Processor row ***" << endl;
-	DoA2A(squarerowcomm, 32*n);
+	DoA2A(squarerowcomm, 32*n, squarecolcomm);
 	if(rank == 0) cout << "*** Processor column ***" << endl;
-	DoA2A(squarecolcomm, 32*n);
+	DoA2A(squarecolcomm, 32*n, squarerowcomm);
 	if(rank == 0) cout << "*** Processor row ***" << endl;
-	DoAG(squarerowcomm, n);
+	DoAG(squarerowcomm, n, squarecolcomm);
 	if(rank == 0) cout << "*** Processor column ***" << endl;
-	DoAG(squarecolcomm, n);
+	DoAG(squarecolcomm, n, squarerowcomm);
 		
 
 	if(rank == 0)
@@ -218,10 +276,10 @@ int main(int argc, char* argv[])
 	myprocrow = rank / tallgrcols;
     	MPI_Comm_split( MPI_COMM_WORLD, myprocrow, rank, &tallrowcomm );
     	MPI_Comm_split( MPI_COMM_WORLD, myproccol, rank, &tallcolcomm );
-	DoA2A(tallrowcomm, 32*n);
-	DoA2A(tallcolcomm, 32*n);
-	DoAG(tallrowcomm, n);
-	DoAG(tallcolcomm, n);
+	DoA2A(tallrowcomm, 32*n, tallcolcomm);
+	DoA2A(tallcolcomm, 32*n, tallrowcomm);
+	DoAG(tallrowcomm, n, tallcolcomm);
+	DoAG(tallcolcomm, n, tallrowcomm);
 
 	if(rank == 0)
 		cout << "### WIDE GRID ###" << endl;
@@ -232,10 +290,10 @@ int main(int argc, char* argv[])
 	myprocrow = rank / widegrcols;
     	MPI_Comm_split( MPI_COMM_WORLD, myprocrow, rank, &widerowcomm );
     	MPI_Comm_split( MPI_COMM_WORLD, myproccol, rank, &widecolcomm );
-	DoA2A(widerowcomm, 32*n);
-	DoA2A(widecolcomm, 32*n);
-	DoAG(widerowcomm, n);
-	DoAG(widecolcomm, n);
+	DoA2A(widerowcomm, 32*n, widecolcomm);
+	DoA2A(widecolcomm, 32*n, widerowcomm);
+	DoAG(widerowcomm, n, widecolcomm);
+	DoAG(widecolcomm, n, widerowcomm);
 
 	MPI_Finalize( );
 	
