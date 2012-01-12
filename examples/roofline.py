10ab216117e04dead2a652cbb87e4bf8a2a90f36
alugowski <alugowski@localhost>
2012-01-12 22:30:47 +0000

    python roofline update

--- examples/roofline.py
+++ examples/roofline.py
@@ -4,7 +4,7 @@ import kdt.pyCombBLAS as pcb
 import time
 
 veclen = [10, 50, 100, 150, 200, 500, 1000, 1300, 1500, 1700]#, 2000, 2500, 3000, 5000, 10000, 15000]	
-repeats = [10, 50, 100, 200, 300, 400]#, 1000, 10000, 100000]
+repeats = [10, 25, 50, 75, 100, 200, 300, 400]#, 1000, 10000, 100000]
 
 best_ops_per_s = 0
 best_len = -1
@@ -23,7 +23,7 @@ for vl in veclen:
 			vec.applyInd(func)
 		t = time.time() - begin
 		ops = vl*r / t
-		kdt.p("len %5d, repeat %3d times: %f"%(vl, r, ops))
+		kdt.p("len %5d, repeat %3d times: %f (took %f seconds)"%(vl, r, ops, t))
 		if best_ops_per_s < ops:
 			best_ops_per_s = ops
 			best_len = vl
