b7272f4d7395b24cad62b606961db22f0aef642f
alugowski <alugowski@localhost>
2012-03-23 05:19:00 +0000

    kdt roofline

--- sandbox/run-roofline.sh
+++ sandbox/run-roofline.sh
@@ -3,6 +3,6 @@
 for proc in 1 4 9 16 25 36 49
 do
 	echo "============================== $proc procs: ==============================="
-	mpirun -np $proc python roofline_spmv_pure.py > foo
-	cat foo | python roofline_pure_parser.py
+	mpirun -np $proc python roofline_spmv_pure.py #> foo
+	#cat foo | python roofline_pure_parser.py
 done
