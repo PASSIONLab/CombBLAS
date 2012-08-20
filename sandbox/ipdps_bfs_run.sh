8d6873813a790a790f66ec685c8b7515b016a48a
alugowski <alugowski@localhost>
2012-08-20 16:25:51 +0000

    run script update

--- sandbox/ipdps_bfs_run.sh
+++ sandbox/ipdps_bfs_run.sh
@@ -4,6 +4,8 @@ for p in 36 25 16 9 4 1
 do
 	for scale in 20
 	do
-		echo "mpirun -np $p python TwitterFilterPercent-combined.py $scale sso cso cpm cpo pso ppo > result_ipdps_bfs_${scale}_${p}.txt"
+		command="mpirun -np $p python TwitterFilterPercent-combined.py $scale sso cso cpm cpo pso ppo > result_ipdps_bfs_${scale}_${p}.txt"
+		echo $command
+		`$command`
 	done
 done
