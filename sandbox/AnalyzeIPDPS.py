d21b81c1687b4ba35a939a72808da7a84caef665
alugowski <alugowski@localhost>
2012-08-25 19:48:01 +0000

    added IPDPS mirasol runs

---
+++ sandbox/AnalyzeIPDPS.py
@@ -0,0 +1,114 @@
+import sys
+import re
+
+cores = {1: "result_ipdps_bfs_22_1.txt", 4: "result_ipdps_bfs_22_4.txt", 9: "result_ipdps_bfs_22_9.txt", 16: "result_ipdps_bfs_22_16.txt", 25: "result_ipdps_bfs_22_25.txt", 36: "result_ipdps_bfs_22_36.txt"}
+
+experiments = ["PythonSR_PythonFilter_OTF", "PythonSR_SejitsFilter_OTF", "C++SR_PythonFilter_OTF","C++SR_SejitsFilter_OTF", "SejitsSR_SejitsFilter_OTF", "C++SR_PythonFilter_Mat"]
+
+# ID will be replaced by strings from experiments array
+experiment_varieties = ["mean_IDtime", "min_IDtime", "max_IDtime"]
+
+varieties = []
+for exp in experiments:
+	for var in experiment_varieties:
+		varieties.append(var.replace("ID", exp))
+
+filters = [1, 10, 25, 100]
+
+data = []
+# parse
+for (core, file) in cores.items():
+	for line in open(file, 'r'):
+		for var in varieties:
+			if line.find(var) != -1:
+				feats = line.split("\t")
+				# var = feats[0]
+				filter = int(float(feats[1]))
+				# : = feats[2]
+				time = float(feats[3])
+				data.append((core, var, filter, time))
+
+# printer
+def format_table(data, group_col_idx, group_col_select_val, col_ids, row_ids, col_idx, row_idx, value_idx):
+	ret = ""
+	fdata = []
+	for i in range(len(row_ids)):
+		fdata.append([])
+		fdata[i].append(row_ids[i])
+		for j in range(len(col_ids)):
+			fdata[i].append("")
+		
+	# format the data into a grid
+	for d in data:
+		if d[group_col_idx] == group_col_select_val:
+			row = row_ids.index(d[row_idx])
+			column = col_ids.index(d[col_idx])+1
+			#print d, row, column, fdata[row][column], "=>", d[value_idx]
+			fdata[row][column] = d[value_idx]
+				
+	# print headers
+	ret += "\t"
+	for val in col_ids:
+		ret += val+"\t"
+	ret += "\n"
+	# print grid
+	for row in fdata:
+		for val in row:
+			ret += str(val) + "\t"
+		ret += "\n"
+	
+	return ret
+
+# print filter by filter
+
+for filter in filters:
+	print ""
+	k = cores.keys()
+	k.sort()
+	print "filter ",filter, k
+	grid = format_table(data, 2, filter, varieties, k, 1, 0, 3)
+	print grid
+
+	filestem = "gnuplot_%d"%filter
+	
+	gnuplot = ""
+	gnuplot += 'set title "Filter %d"\n'%(filter)
+	gnuplot += 'set terminal png\n'
+	gnuplot += 'set output "%s.png"\n'%filestem
+	gnuplot += ''
+	gnuplot += 'set xrange [0:40]\n'
+	gnuplot += 'set yrange [0:20]\n'
+	gnuplot += "set xlabel 'number of MPI processes'\n"
+	gnuplot += "set ylabel 'mean BFS time (s)'\n"
+	
+	xtics = ""
+	cc = cores.keys()
+	for i in range(len(cc)):
+		if i+1 < len(cc):
+			comma = ", "
+		else:
+			comma = ""
+		xtics += "'%d' %d%s"%(cc[i], cc[i], comma)
+	
+	gnuplot += "set xtics (%s)\n"%xtics
+	gnuplot += 'plot\\\n'
+	vars_per_exp = len(experiment_varieties)
+	for i in range(len(experiments)):
+		exp_col_start = 1 + vars_per_exp*i + 1
+		if i+1 < len(experiments):
+			comma = ",\\"
+		else:
+			comma = ""
+		gnuplot += ' "%s.dat" every ::1 using 1:%d:%d:%d title \'\' ps 0 lc rgb \'black\' with errorbars,\\\n'%(filestem, exp_col_start,exp_col_start+1,exp_col_start+2)
+		gnuplot += ' "%s.dat" every ::1 using 1:%d title \'%s\' with lines%s\n'%(filestem, exp_col_start, experiments[i], comma)
+
+	print ""
+	print gnuplot
+	
+	f = open('%s.dat'%filestem, 'w')
+	f.write(grid)
+	f.close()
+
+	f = open('%s.gp'%filestem, 'w')
+	f.write(gnuplot)
+	f.close()
---
+++ sandbox/ipdps_results/results-mirasol-bfs/gnuplot_25.gp
@@ -0,0 +1,21 @@
+set title "Filter 25"
+set terminal png
+set output "gnuplot_25.png"
+set xrange [0:40]
+set yrange [0:20]
+set xlabel 'number of MPI processes'
+set ylabel 'mean BFS time (s)'
+set xtics ('1' 1, '36' 36, '9' 9, '16' 16, '25' 25, '4' 4)
+plot\
+ "gnuplot_25.dat" every ::1 using 1:2:3:4 title '' ps 0 lc rgb 'black' with errorbars,\
+ "gnuplot_25.dat" every ::1 using 1:2 title 'PythonSR_PythonFilter_OTF' with lines,\
+ "gnuplot_25.dat" every ::1 using 1:5:6:7 title '' ps 0 lc rgb 'black' with errorbars,\
+ "gnuplot_25.dat" every ::1 using 1:5 title 'PythonSR_SejitsFilter_OTF' with lines,\
+ "gnuplot_25.dat" every ::1 using 1:8:9:10 title '' ps 0 lc rgb 'black' with errorbars,\
+ "gnuplot_25.dat" every ::1 using 1:8 title 'C++SR_PythonFilter_OTF' with lines,\
+ "gnuplot_25.dat" every ::1 using 1:11:12:13 title '' ps 0 lc rgb 'black' with errorbars,\
+ "gnuplot_25.dat" every ::1 using 1:11 title 'C++SR_SejitsFilter_OTF' with lines,\
+ "gnuplot_25.dat" every ::1 using 1:14:15:16 title '' ps 0 lc rgb 'black' with errorbars,\
+ "gnuplot_25.dat" every ::1 using 1:14 title 'SejitsSR_SejitsFilter_OTF' with lines,\
+ "gnuplot_25.dat" every ::1 using 1:17:18:19 title '' ps 0 lc rgb 'black' with errorbars,\
+ "gnuplot_25.dat" every ::1 using 1:17 title 'C++SR_PythonFilter_Mat' with lines
