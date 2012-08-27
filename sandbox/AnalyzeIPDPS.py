642a7555d24a3196f3d679a0a214ea47b0ff0013
alugowski <alugowski@localhost>
2012-08-27 16:22:57 +0000

    updated plots

--- sandbox/AnalyzeIPDPS.py
+++ sandbox/AnalyzeIPDPS.py
@@ -9,18 +9,49 @@ filters = [1, 10, 25, 100]
 
 if sys.argv[1] == "bfs":
 	cores = {1: "result_ipdps_bfs_22_1.txt", 4: "result_ipdps_bfs_22_4.txt", 9: "result_ipdps_bfs_22_9.txt", 16: "result_ipdps_bfs_22_16.txt", 25: "result_ipdps_bfs_22_25.txt", 36: "result_ipdps_bfs_22_36.txt"}
+	combblas_file = "result_ipdps_bfs_22_combblas.txt"
+	# combblas file format: each line is a tab-delimited tuple:
+	# core count, filter percentage, min time, max time, mean time
 	
-	experiments = ["PythonSR_PythonFilter_OTF", "PythonSR_SejitsFilter_OTF", "C++SR_PythonFilter_OTF","C++SR_SejitsFilter_OTF", "SejitsSR_SejitsFilter_OTF", "C++SR_PythonFilter_Mat"]
+	experiments = [("PythonSR_PythonFilter_OTF", "Python/Python KDT", "#FF0000"), # red (kinda light)
+		("PythonSR_SejitsFilter_OTF", "Python/SEJITS KDT", "#8B0000"), # dark red
+		("C++SR_PythonFilter_OTF", "C++/Python KDT", "#90EE90"), # light green
+		("C++SR_SejitsFilter_OTF", "C++/SEJITS KDT", "#008000"), # green (but it's dark)
+		("SejitsSR_SejitsFilter_OTF", "SEJITS/SEJITS KDT", "#0000FF"), # blue (but it's dark)
+		("CombBLAS_OTF", "C++/C++ CombBLAS", "#FFD700"), # gold
+		("C++SR_PythonFilter_Mat", "C++/Python KDT (materialized)", "#000000")] # black
 	
 	# ID will be replaced by strings from experiments array
 	experiment_varieties = ["mean_IDtime", "min_IDtime", "max_IDtime"]
+	
+	result_type = "BFS"
+	
+	def parseCombBLAS(data):
+		for line in open(combblas_file, 'r'):
+			feats = line.split("\t")
+			core = int(feats[0])
+			filter = int(float(feats[1]))
+			min_time = float(feats[2])
+			max_time = float(feats[3])
+			mean_time = float(feats[4])
+			data.append((core, "min_CombBLAS_OTFtime", filter, min_time))
+			data.append((core, "max_CombBLAS_OTFtime", filter, max_time))
+			data.append((core, "mean_CombBLAS_OTFtime", filter, mean_time))
+		
 elif sys.argv[1] == "mis":
 	cores = {1: "result_ipdps_MIS_1.txt", 4: "result_ipdps_MIS_4.txt", 9: "result_ipdps_MIS_9.txt", 16: "result_ipdps_MIS_16.txt", 25: "result_ipdps_MIS_25.txt", 36: "result_ipdps_MIS_36.txt"}
 	
-	experiments = ["PythonSR_PythonFilter_ER_OTF_22", "PythonSR_SejitsFilter_ER_OTF_22", "SejitsSR_SejitsFilter_ER_OTF_22"]
+	experiments = [("PythonSR_PythonFilter_ER_OTF_22", "Python/Python KDT", "#FF0000"), # red (kinda light)
+				("PythonSR_SejitsFilter_ER_OTF_22", "Python/SEJITS KDT", "#8B0000"), # dark red
+				("SejitsSR_SejitsFilter_ER_OTF_22", "SEJITS/SEJITS KDT", "#008000")] # blue (but it's dark)
 	
 	# ID will be replaced by strings from experiments array
 	experiment_varieties = ["mean_IDtime", "min_IDtime", "max_IDtime"]
+
+	result_type = "MIS"
+
+	def parseCombBLAS(data):
+		pass
 else:
 	print "unknown option. use bfs or mis"
 	sys.exit()
@@ -28,7 +59,7 @@ else:
 varieties = []
 for exp in experiments:
 	for var in experiment_varieties:
-		varieties.append(var.replace("ID", exp))
+		varieties.append(var.replace("ID", exp[0]))
 
 
 data = []
@@ -43,6 +74,8 @@ for (core, file) in cores.items():
 				# : = feats[2]
 				time = float(feats[3])
 				data.append((core, var, filter, time))
+
+parseCombBLAS(data)
 
 # printer
 def format_table(data, group_col_idx, group_col_select_val, col_ids, row_ids, col_idx, row_idx, value_idx):
@@ -88,7 +121,7 @@ for filter in filters:
 	filestem = "gnuplot_%d"%filter
 	
 	gnuplot = ""
-	gnuplot += 'set title "Filter %d"\n'%(filter)
+	gnuplot += 'set title "Filtered %s (%d%% permeability)"\n'%(result_type, filter)
 	gnuplot += 'set terminal png\n'
 	gnuplot += 'set output "%s.png"\n'%filestem
 	gnuplot += ''
@@ -96,7 +129,7 @@ for filter in filters:
 	gnuplot += 'set yrange [0.1:256]\n'
 	gnuplot += 'set logscale y\n'
 	gnuplot += "set xlabel 'number of MPI processes'\n"
-	gnuplot += "set ylabel 'mean BFS time (s)'\n"
+	gnuplot += "set ylabel 'mean %s time (seconds, log scale)'\n"%(result_type)
 	
 	xtics = ""
 	cc = cores.keys()
@@ -116,8 +149,8 @@ for filter in filters:
 			comma = ",\\"
 		else:
 			comma = ""
-		gnuplot += ' "%s.dat" every ::1 using 1:%d:%d:%d title \'\' ps 0 lc rgb \'black\' with errorbars,\\\n'%(filestem, exp_col_start,exp_col_start+1,exp_col_start+2)
-		gnuplot += ' "%s.dat" every ::1 using 1:%d title \'%s\' with lines%s\n'%(filestem, exp_col_start, experiments[i], comma)
+		gnuplot += ' "%s.dat" every ::1 using 1:%d:%d:%d title \'\' ps 0 lc rgb \'%s\' with errorbars,\\\n'%(filestem, exp_col_start,exp_col_start+1,exp_col_start+2, experiments[i][2])
+		gnuplot += ' "%s.dat" every ::1 using 1:%d title \'%s\' lc rgb \'%s\' with lines%s\n'%(filestem, exp_col_start, experiments[i][1], experiments[i][2], comma)
 
 	print ""
 	print gnuplot
--- sandbox/ipdps_results/results-mirasol-bfs/gnuplot_100.gp
+++ sandbox/ipdps_results/results-mirasol-bfs/gnuplot_100.gp
@@ -1,22 +1,24 @@
-set title "Filter 100"
+set title "Filtered BFS (100% permeability)"
 set terminal png
 set output "gnuplot_100.png"
 set xrange [0:40]
 set yrange [0.1:256]
 set logscale y
 set xlabel 'number of MPI processes'
-set ylabel 'mean BFS time (s)'
+set ylabel 'mean BFS time (seconds, log scale)'
 set xtics ('1' 1, '36' 36, '9' 9, '16' 16, '25' 25, '4' 4)
 plot\
- "gnuplot_100.dat" every ::1 using 1:2:3:4 title '' ps 0 lc rgb 'black' with errorbars,\
- "gnuplot_100.dat" every ::1 using 1:2 title 'PythonSR_PythonFilter_OTF' with lines,\
- "gnuplot_100.dat" every ::1 using 1:5:6:7 title '' ps 0 lc rgb 'black' with errorbars,\
- "gnuplot_100.dat" every ::1 using 1:5 title 'PythonSR_SejitsFilter_OTF' with lines,\
- "gnuplot_100.dat" every ::1 using 1:8:9:10 title '' ps 0 lc rgb 'black' with errorbars,\
- "gnuplot_100.dat" every ::1 using 1:8 title 'C++SR_PythonFilter_OTF' with lines,\
- "gnuplot_100.dat" every ::1 using 1:11:12:13 title '' ps 0 lc rgb 'black' with errorbars,\
- "gnuplot_100.dat" every ::1 using 1:11 title 'C++SR_SejitsFilter_OTF' with lines,\
- "gnuplot_100.dat" every ::1 using 1:14:15:16 title '' ps 0 lc rgb 'black' with errorbars,\
- "gnuplot_100.dat" every ::1 using 1:14 title 'SejitsSR_SejitsFilter_OTF' with lines,\
- "gnuplot_100.dat" every ::1 using 1:17:18:19 title '' ps 0 lc rgb 'black' with errorbars,\
- "gnuplot_100.dat" every ::1 using 1:17 title 'C++SR_PythonFilter_Mat' with lines
+ "gnuplot_100.dat" every ::1 using 1:2:3:4 title '' ps 0 lc rgb '#FF0000' with errorbars,\
+ "gnuplot_100.dat" every ::1 using 1:2 title 'Python/Python KDT' lc rgb '#FF0000' with lines,\
+ "gnuplot_100.dat" every ::1 using 1:5:6:7 title '' ps 0 lc rgb '#8B0000' with errorbars,\
+ "gnuplot_100.dat" every ::1 using 1:5 title 'Python/SEJITS KDT' lc rgb '#8B0000' with lines,\
+ "gnuplot_100.dat" every ::1 using 1:8:9:10 title '' ps 0 lc rgb '#90EE90' with errorbars,\
+ "gnuplot_100.dat" every ::1 using 1:8 title 'C++/Python KDT' lc rgb '#90EE90' with lines,\
+ "gnuplot_100.dat" every ::1 using 1:11:12:13 title '' ps 0 lc rgb '#008000' with errorbars,\
+ "gnuplot_100.dat" every ::1 using 1:11 title 'C++/SEJITS KDT' lc rgb '#008000' with lines,\
+ "gnuplot_100.dat" every ::1 using 1:14:15:16 title '' ps 0 lc rgb '#0000FF' with errorbars,\
+ "gnuplot_100.dat" every ::1 using 1:14 title 'SEJITS/SEJITS KDT' lc rgb '#0000FF' with lines,\
+ "gnuplot_100.dat" every ::1 using 1:17:18:19 title '' ps 0 lc rgb '#FFD700' with errorbars,\
+ "gnuplot_100.dat" every ::1 using 1:17 title 'C++/C++ CombBLAS' lc rgb '#FFD700' with lines,\
+ "gnuplot_100.dat" every ::1 using 1:20:21:22 title '' ps 0 lc rgb '#000000' with errorbars,\
+ "gnuplot_100.dat" every ::1 using 1:20 title 'C++/Python KDT (materialized)' lc rgb '#000000' with lines
