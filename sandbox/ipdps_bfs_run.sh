ebb127fadbfe7bb49b9b499adc8054c56dca3867
alugowski <alugowski@localhost>
2012-09-13 20:41:26 +0000

    updated mis numbers

--- sandbox/ipdps_bfs_run.sh
+++ sandbox/ipdps_bfs_run.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
-for p in 9 4 1 #36 25 16 9 4 1
+for p in 36 25 #16 9 4 1
 do
 	for scale in 22
 	do
-		command="mpirun -np $p python TwitterFilterPercent-combined.py $scale sso cso cpm cpo pso ppo > result_ipdps_bfs_${scale}_${p}.txt"
+		command="mpirun -np $p python TwitterFilterPercent-combined.py $scale sso pso ppo > result_ipdps_bfs_${scale}_${p}.txt"
 		echo $command
-		#mpirun -np $p python TwitterFilterPercent-combined.py $scale sso cso cpm cpo pso ppo > result_ipdps_bfs_${scale}_${p}.txt
+		mpirun -np $p python TwitterFilterPercent-combined.py $scale sso pso ppo > result_ipdps_bfs_${scale}_${p}.txt
 	done
 done
