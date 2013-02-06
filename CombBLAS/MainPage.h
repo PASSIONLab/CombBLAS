d344a36828c69291c7afb2f9f7f68a72cc9af61e
aydin <aydin@localhost>
2013-02-06 09:48:03 +0000

    shitload of stuff

--- CombBLAS/MainPage.h
+++ CombBLAS/MainPage.h
@@ -12,18 +12,18 @@
 * Knowledge Discovery Toolbox (KDT) </a>. 
 * - It achieves scalability via its two dimensional distribution and coarse-grained parallelism. 
 * - For an illustrative overview, check out <a href="http://gauss.cs.ucsb.edu/~aydin/talks/CombBLAS_Nov11.pdf">these slides</a>. 
-* - Check out the <a href="FAQ-combblas.html">Frequently asked questions about CombBLAS</a>. 
+* - Check out the <a href="http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/FAQ-combblas.html">Frequently asked questions about CombBLAS</a>.
 *
 * <b>Download</b> 
-* - Read release notes <a href="../release-notes.html">here</a>.
-* - The latest CMake'd tarball (version 1.3.0, Feb 2013) <a href="http://gauss.cs.ucsb.edu/code/CombBLAS/CombBLAS_beta_13_0.tar.gz"> here</a>. (NERSC users read <a href="http://gauss.cs.ucsb.edu/code/CombBLAS/NERSC_INSTALL.html">this</a>). 
- The previous version (version 1.2.1, April 2012) is also available <a href="http://gauss.cs.ucsb.edu/code/CombBLAS/CombBLAS_beta_12_1.tgz"> here </a> for backwards compatibility and benchmarking. 
+* - Read <a href="http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/release-notes.html">release notes</a>.
+* - The latest CMake'd tarball (version 1.3.0, Feb 2013) <a href="http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/CombBLAS_beta_13_0.tgz"> here</a>. (NERSC users read <a href="http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/NERSC_INSTALL.html">this</a>). 
+ The previous version (version 1.2.1, April 2012) is also available <a href="http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/CombBLAS_beta_12_1.tgz"> here </a> for backwards compatibility and benchmarking. 
 * 	- To create sample applications
 * and run simple tests, all you need to do is to execute the following three commands, in the given order, inside the main directory: 
 * 		-  <i> cmake . </i>
 * 		- <i> make </i>
 * 		- <i> ctest -V </i> (you need the testinputs, see below)
-* 	- Test inputs are separately downloadable <a href="http://gauss.cs.ucsb.edu/code/CombBLAS/testdata_combblas1.2.tar.gz"> here</a>. Extract them inside the CombBLAS_vx.x directory with the command "tar -xzvf testdata_combblas1.2.tar.gz"
+* 	- Test inputs are separately downloadable <a href="http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/testdata_combblas1.2.1.tgz"> here</a>. Extract them inside the CombBLAS_vx.x directory with the command "tar -xzvf testdata_combblas1.2.tar.gz"
 * - Alternatively (if cmake fails, or you just don't want to install it), you can just imitate the sample makefiles inside the ReleaseTests and Applications 
 * directories. Those sample makefiles have the following format: makefile-<i>machine</i>. (example: makefile-neumann) 
 * 
@@ -44,7 +44,7 @@
 *
 * The implementation supports both formatted and binary I/O. The latter is much faster but no human readable. Formatted I/O uses a tuples format very similar to the Matrix Market.
 * We encourage in-memory generators for faster benchmarking. A port to University of Florida Sparse Matrix Collection is under construction. More info on I/O formats are 
-* <a href="http://gauss.cs.ucsb.edu/code/CombBLAS/Input_File_Formats.pdf">here</a>
+* <a href="http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/Input_File_Formats.pdf">here</a>
 *
 * The main data structure is a distributed sparse matrix ( SpParMat <IT,NT,DER> ) which HAS-A sequential sparse matrix ( SpMat <IT,NT> ) that 
 * can be implemented in various ways as long as it supports the interface of the base class (currently: SpTuples, SpCCols, SpDCCols).
@@ -61,11 +61,10 @@
 * (sparse matrix-dense vector multiplication) 
 *
 * <b> New in version 1.3</b>: 
-* - MPI C++ bindings are removed since they are no longer supported my MPI-3 standard. All MPI calls are C-style. 
+* - MPI C++ bindings are removed since they are no longer supported by the MPI-3 standard. All MPI calls are C-style. 
 * - On-the-fly filtering support [7] illustrated through examples (FilteredBFS.cpp, FilteredMIS.cpp, and TwitterEdge.h). 
-* - Frequently asked questions (FAQ) added. 
+* - <a href="http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/FAQ-combblas.html">Frequently asked questions (FAQ)</a> added. 
 * - Maximal Independent Set (MIS) application implememented in the language of linear algebra (FilteredMIS.cpp).
-* - 
 *
 * The supported operations (a growing list) are:
 * - Sparse matrix-matrix multiplication on a semiring SR: PSpGEMM()
@@ -92,7 +91,7 @@
 * - Reference counting using shared_ptr for IITO (implemented in terms of) relationships
 * - As external code, it utilizes 
 *	- sequence heaps of <a href="http://www.mpi-inf.mpg.de/~sanders/programs/"> Peter Sanders </a>.
-*	- a modified (memory efficient) version of the Viral Shah's <a href="http://gauss.cs.ucsb.edu/~viral/PAPERS/psort/html/psort.html"> PSort </a>.
+*	- a modified (memory efficient) version of the Viral Shah's <a href="http://www.allthingshpc.org/Publications/psort/psort.html"> PSort </a>.
 *	- a modified version of the R-MAT generator from <a href="http://graph500.org"> Graph 500 reference implementation </a>
 * 
 * Important Sequential classes:
@@ -108,14 +107,14 @@
 *
 * 
 * <b> Applications </b>  implemented using Combinatorial BLAS:
-* - BetwCent.cpp : Betweenness centrality computation on directed, unweighted graphs. Download sample input <a href=" http://gauss.cs.ucsb.edu/code/CombBLAS/scale17_bc_inp.tar.gz"> here </a>.
+* - BetwCent.cpp : Betweenness centrality computation on directed, unweighted graphs. Download sample input <a href=" http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/scale17_bc_inp.tar.gz"> here </a>.
 * - MCL.cpp : An implementation of the MCL graph clustering algorithm.
 * - Graph500.cpp: A conformant implementation of the <a href="http://graph500.org">Graph 500 benchmark</a>.
 * - FilteredMIS.cpp: Filtered maximal independent set calculation on ER graphs using Luby's algorithm. 
 * - FilteredBFS.cpp: Filtered breadth-first search on a twitter-like data set. 
 *
 * <b> Performance </b> results of the first two applications can be found in the design paper [1]; Graph 500 results are in a recent BFS paper [4]. The most
-recent sparse matrix indexing, assignment, and multiplication results can be found in [5].
+recent sparse matrix indexing, assignment, and multiplication results can be found in [5]. Performance of filtered graph algorithms (BFS and MIS) are reported in [7]. 
 *
 * A subset of test programs demonstrating how to use the library (under ReleaseTests):
 * - TransposeTest.cpp : File I/O and parallel transpose tests
@@ -130,8 +129,9 @@ recent sparse matrix indexing, assignment, and multiplication results can be fou
 * - [1] Aydın Buluç and John R. Gilbert, <i> The Combinatorial BLAS: Design, implementation, and applications </i>. International Journal of High Performance Computing Applications (IJHPCA), 2011. <a href="http://www.cs.ucsb.edu/research/tech_reports/reports/2010-18.pdf"> Preprint </a>, <a href="http://hpc.sagepub.com/content/early/2011/05/11/1094342011403516.abstract">Link</a>
 * - [2] Aydın Buluç and John R. Gilbert, <i> On the Representation and Multiplication of Hypersparse Matrices </i>. The 22nd IEEE International Parallel and Distributed Processing Symposium (IPDPS 2008), Miami, FL, April 14-18, 2008
 * - [3] Aydın Buluç and John R. Gilbert, <i> Challenges and Advances in Parallel Sparse Matrix-Matrix Multiplication </i>. The 37th International Conference on Parallel Processing (ICPP 2008), Portland, Oregon, USA, 2008
-* - [4] Aydın Buluç and Kamesh Madduri, <i> Parallel Breadth-First Search on Distributed-Memory Systems </i>. Supercomputing (SC'11), Seattle, USA. <a href="http://arxiv.org/abs/1104.4518">Extended preprint</a>
+* - [4] Aydın Buluç and Kamesh Madduri, <i> Parallel Breadth-First Search on Distributed-Memory Systems </i>. Supercomputing (SC'11), Seattle, USA. <a href="http://arxiv.org/abs/1104.4518">Extended preprint</a> <a href="http://gauss.cs.ucsb.edu/~aydin/sc11_bfs.pdf"> PDF </a>
 * - [5] Aydın Buluç and John R. Gilbert. <i> Parallel Sparse Matrix-Matrix Multiplication and Indexing: Implementation and Experiments </i>. SIAM Journal of Scientific Computing, 2012. <a href="http://arxiv.org/abs/1109.3739"> Preprint </a>
+ <a href="http://gauss.cs.ucsb.edu/~aydin/spgemm_sisc12.pdf"> PDF </a>
 * - [6] Aydın Buluç. <i> Linear Algebraic Primitives for Computation on Large Graphs </i>. PhD thesis, University of California, Santa Barbara, 2010. <a href="http://gauss.cs.ucsb.edu/~aydin/Buluc_Dissertation.pdf"> PDF </a>
-* - [7] Aydın Buluç, Erika Duriakova, Armando Fox, John Gilbert, Shoaib Kamil, Adam Lugowski, Leonid Oliker, Samuel Williams. <i> High-Productivity and High-Performance Analysis of Filtered Semantic Graphs </i> , International Parallel and Distributed Processing Symposium (IPDPS), 2013.
+* - [7] Aydın Buluç, Erika Duriakova, Armando Fox, John Gilbert, Shoaib Kamil, Adam Lugowski, Leonid Oliker, Samuel Williams. <i> High-Productivity and High-Performance Analysis of Filtered Semantic Graphs </i> , International Parallel and Distributed Processing Symposium (IPDPS), 2013. <a href="http://gauss.cs.ucsb.edu/~aydin/ipdps13-kdtsejits.pdf"> PDF </a>
 */
