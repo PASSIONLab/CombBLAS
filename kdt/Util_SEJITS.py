9ce108432dd01ac3fb4f9de6d5bc9e42ecce840f
alugowski <alugowski@localhost>
2013-04-02 19:00:07 +0000

    sejits util update

--- kdt/Util_SEJITS.py
+++ kdt/Util_SEJITS.py
@@ -3,25 +3,29 @@
 # SEJITS itself is in the specializer subdirectory.
 
 
-# placeholders for when SEJITS is disabled
+# placeholder parent class for when SEJITS is disabled
 class _SEJITS_diabled_callback_parent:
 	pass
 
+# the SEJITS callback parent classes
 KDTUnaryPredicate = _SEJITS_diabled_callback_parent
 KDTBinaryPredicate = _SEJITS_diabled_callback_parent
 KDTUnaryFunction = _SEJITS_diabled_callback_parent
 KDTBinaryFunction = _SEJITS_diabled_callback_parent
 
+# load the real SEJITS callbacks
 try:
 	from specializer.pcb_predicate import PcbUnaryPredicate
 	from specializer.pcb_predicate import PcbBinaryPredicate
 	from specializer.pcb_function import PcbUnaryFunction
 	from specializer.pcb_function import PcbBinaryFunction
-	print "here"
 except ImportError:
 	pass
-	
+
 def SEJITS_enable(en):
+"""
+Enables/disables SEJITS by manipulating the callback parent classes.
+"""
 	global KDTUnaryPredicate
 	global KDTBinaryPredicate
 	global KDTUnaryFunction
@@ -42,4 +46,7 @@ def SEJITS_enable(en):
 		KDTBinaryFunction = _SEJITS_diabled_callback_parent
 
 def SEJITS_enabled():
+"""
+Tests whether or not the SEJITS callback parent classes are set or not.
+"""
 	return KDTUnaryPredicate is not _SEJITS_diabled_callback_parent
