ed93e6fcacc01fa6e75e2fbf80cb6f68838a537d
alugowski <alugowski@localhost>
2013-04-02 17:07:32 +0000

    update

--- kdt/Util_SEJITS.py
+++ kdt/Util_SEJITS.py
@@ -12,7 +12,21 @@ KDTBinaryPredicate = _SEJITS_diabled_callback_parent
 KDTUnaryFunction = _SEJITS_diabled_callback_parent
 KDTBinaryFunction = _SEJITS_diabled_callback_parent
 
+try:
+	from specializer.pcb_predicate import PcbUnaryPredicate
+	from specializer.pcb_predicate import PcbBinaryPredicate
+	from specializer.pcb_function import PcbUnaryFunction
+	from specializer.pcb_function import PcbBinaryFunction
+	print "here"
+except ImportError:
+	pass
+	
 def SEJITS_enable(en):
+	global KDTUnaryPredicate
+	global KDTBinaryPredicate
+	global KDTUnaryFunction
+	global KDTBinaryFunction
+
 	if en:
 		try:
 			KDTUnaryPredicate = PcbUnaryPredicate
