c63b24eec060da8369ec50122bafe0af98acd44c
alugowski <alugowski@localhost>
2013-06-27 18:07:49 +0000

    papi sync

--- sandbox/papi.py
+++ sandbox/papi.py
@@ -4,6 +4,12 @@ from ctypes import *
 cdll.LoadLibrary("libpapi.so.4")
 libpapi = CDLL("libpapi.so.4")
 
-def get_papi_event_list(str_events):
-	pass
+print libpapi['PAPI_VER_CURRENT']
+
+def PAPI_event_name_to_code(EventName):
+    code = c_int()
+    error = libpapi.PAPI_event_name_to_code(c_char_p(str(EventName)), byref(code));
+    print "error:",error
+    return code.value
+
 
