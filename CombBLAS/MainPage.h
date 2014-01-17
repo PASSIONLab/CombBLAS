967849179f3be4db7e41a05ef6433ebfc3a3b638
aydin <aydin@localhost>
2014-01-17 01:39:41 +0000

    ttt

--- CombBLAS/MainPage.h
+++ CombBLAS/MainPage.h
@@ -114,7 +114,7 @@
 * - BetwCent.cpp : Betweenness centrality computation on directed, unweighted graphs. Download sample input <a href=" http://gauss.cs.ucsb.edu/~aydin/CombBLAS_FILES/scale17_bc_inp.tar.gz"> here </a>.
 * - MCL.cpp : An implementation of the MCL graph clustering algorithm.
 * - TopDownBFS.cpp: A conformant implementation of the <a href="http://graph500.org">Graph 500 benchmark</a> using the traditional top-down BFS.
-* - DirOptBFS.cpp: A conformant implementation of the <a href="http://graph500.org">Graph 500 benchmark</a> using the traditional the faster direction-optimizing BFS.
+* - DirOptBFS.cpp: A conformant implementation of the <a href="http://graph500.org">Graph 500 benchmark</a> using the faster direction-optimizing BFS.
 * - FilteredMIS.cpp: Filtered maximal independent set calculation on ER graphs using Luby's algorithm. 
 * - FilteredBFS.cpp: Filtered breadth-first search on a twitter-like data set. 
 *
