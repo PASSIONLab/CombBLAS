4118ef4c050c7d8aa826361e5a7347cc768f274b
aazad <aazad@localhost>
2014-05-29 21:30:11 +0000

    updated BPMaximalMatching

--- CombBLAS/Applications/BPMaximalMatching.cpp
+++ CombBLAS/Applications/BPMaximalMatching.cpp
@@ -15,6 +15,7 @@
 #endif
 //#define DEBUG
 
+
 #ifdef _OPENMP
 #include <omp.h>
 #endif
@@ -34,6 +35,27 @@ int cblas_splits = 1;
 #define EDGEFACTOR 16
 using namespace std;
 
+// 64-bit floor(log2(x)) function 
+// note: least significant bit is the "zeroth" bit
+// pre: v > 0
+unsigned int highestbitset(uint64_t v)
+{
+	// b in binary is {10,1100, 11110000, 1111111100000000 ...}  
+	const uint64_t b[] = {0x2ULL, 0xCULL, 0xF0ULL, 0xFF00ULL, 0xFFFF0000ULL, 0xFFFFFFFF00000000ULL};
+	const unsigned int S[] = {1, 2, 4, 8, 16, 32};
+	int i;
+
+	unsigned int r = 0; // result of log2(v) will go here
+	for (i = 5; i >= 0; i--) 
+	{
+		if (v & b[i])	// highestbitset is on the left half (i.e. v > S[i] for sure)
+		{
+			v >>= S[i];
+			r |= S[i];
+		} 
+	}
+	return r;
+}
 
 template <class T>
 bool from_string(T & t, const string& s, std::ios_base& (*f)(std::ios_base&))
@@ -52,6 +74,18 @@ void Symmetricize(PARMAT & A)
 	AT.Transpose();
 	A += AT;
 }
+
+
+template<typename T>
+struct positive : public std::unary_function<T, bool>
+{
+    bool operator()(const T& x) const
+    {
+        return (x>0);
+    }
+};
+
+
 
 /**
  * Binary function to prune the previously discovered vertices from the current frontier 
@@ -78,6 +112,7 @@ void sanityCheck(FullyDistVec<IT,NT> & mateCol2Row, FullyDistVec<IT,NT> & mateRo
     int myrank;
     MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
    
+    /*
     // first check if this is a matching
     for(int i=0; i< mateRow2Col.glen ; i++)
     {
@@ -101,7 +136,7 @@ void sanityCheck(FullyDistVec<IT,NT> & mateCol2Row, FullyDistVec<IT,NT> & mateRo
             break;
         }
     }
-    
+    */
     
     // Check if this is a maximal matching
     
@@ -162,6 +197,10 @@ int main(int argc, char* argv[])
 		PSpMat_s32p64 Aeff;
 		FullyDistVec<int64_t, int64_t> degrees;	// degrees of vertices (including multi-edges and self-loops)
 		FullyDistVec<int64_t, int64_t> nonisov;	// id's of non-isolated (connected) vertices
+        
+        FullyDistVec<int64_t, int64_t> nonisoRowV;	// id's of non-isolated (connected) Row vertices
+        FullyDistVec<int64_t, int64_t> nonisoColV;	// id's of non-isolated (connected) Col vertices
+        
 		unsigned scale;
 		OptBuf<int32_t, int64_t> optbuf;	// let indices be 32-bits
 		bool scramble = false;
@@ -341,31 +380,42 @@ int main(int argc, char* argv[])
 			//loopinfo << "Converted to Boolean and removed " << removed << " loops" << endl;
 			//SpParHelper::Print(loopinfo.str());
 			//A.PrintInfo();
-
+            
+            // =========================================================================================================
+            // Remove Isolated vertice
 			FullyDistVec<int64_t, int64_t> * ColSums = new FullyDistVec<int64_t, int64_t>(A.getcommgrid());
 			FullyDistVec<int64_t, int64_t> * RowSums = new FullyDistVec<int64_t, int64_t>(A.getcommgrid());
 			A.Reduce(*ColSums, Column, plus<int64_t>(), static_cast<int64_t>(0)); 	
 			A.Reduce(*RowSums, Row, plus<int64_t>(), static_cast<int64_t>(0)); 	
 			SpParHelper::Print("Reductions done\n");
 			ColSums->EWiseApply(*RowSums, plus<int64_t>());
-			delete RowSums;
-			SpParHelper::Print("Intersection of colsums and rowsums found\n");
+			nonisov = ColSums->FindInds(bind2nd(greater<int64_t>(), 0));
+            
+            
+            //nonisoRowV.iota(A.getnrow(), 0);  // this is not working! making the randpermutation load imbalanced
+            //nonisoColV = ColSums->FindInds(bind2nd(greater<int64_t>(), -1)); // not working either
+            //nonisoColV.iota(A.getncol(), 0);  //
+            //nonisoRowV.RandPerm();
+            //nonisoColV.RandPerm();
+            delete ColSums;
+            delete RowSums;
 
-			nonisov = ColSums->FindInds(bind2nd(greater<int64_t>(), 0));	// only the indices of non-isolated vertices
-			delete ColSums;
-
-			SpParHelper::Print("Found (and permuted) non-isolated vertices\n");	
+			//SpParHelper::Print("Found (and permuted) non-isolated vertices\n");
 			nonisov.RandPerm();	// so that A(v,v) is load-balanced (both memory and time wise)
-			A.PrintInfo();
+            
 #ifndef NOPERMUTE
-			A(nonisov, nonisov, true);	// in-place permute to save memory	
-			SpParHelper::Print("Dropped isolated vertices from input\n");	
+            
+			A(nonisov, nonisov, true);	// in-place permute to save memory
+            //A(nonisoRowV, nonisoColV, true); // there is a problem with this in multi-processor !!
+			//SpParHelper::Print("Dropped isolated vertices from input\n");
 			A.PrintInfo();
 #endif
-
+            // =========================================================================================================
+            
+            
 			Aeff = PSpMat_s32p64(A);	// Convert to 32-bit local integers
 			A.FreeMemory();
-			Symmetricize(Aeff);	// A += A';   ######  BFS depends on this ????
+			//Symmetricize(Aeff);	// A += A';   ######  BFS depends on this ????
 			//SpParHelper::Print("Symmetricized\n");	
 
 			Aeff.OptimizeForGraph500(optbuf);		// Should be called before threading is activated
@@ -375,7 +425,7 @@ int main(int argc, char* argv[])
 			SpParHelper::Print(tinfo.str());
 			Aeff.ActivateThreading(cblas_splits);	
 #endif
-			Aeff.PrintInfo();
+			//Aeff.PrintInfo();
 
 			MPI_Barrier(MPI_COMM_WORLD);
 			double t2=MPI_Wtime();
@@ -384,10 +434,8 @@ int main(int argc, char* argv[])
 			k1timeinfo << (t2-t1) - (redtf-redts) << " seconds elapsed for Kernel #1" << endl;
 			SpParHelper::Print(k1timeinfo.str());
 		}
-#ifdef DEBUG
-cout<< "debug\n";
-#endif
-		Aeff.PrintInfo();
+
+		//Aeff.PrintInfo();
 		float balance = Aeff.LoadImbalance();
 		ostringstream outs;
 		outs << "Load balance: " << balance << endl;
@@ -402,83 +450,122 @@ cout<< "debug\n";
 		degrees.PrintInfo("Degrees array");
 #endif
         
+        
         double tStart;
-
+        
         {
-       
-		MPI_Pcontrol(1,"BFS");
-		//FullyDistVec<int64_t, int64_t> parents ( Aeff.getcommgrid(), Aeff.getncol(), (int64_t) 1);	// identity is -1
-		// ?? what to do for rectanguar matrix ?
-		FullyDistVec<int64_t, int64_t> mateRow2Col ( Aeff.getcommgrid(), Aeff.getnrow(), (int64_t) -1);
-        FullyDistVec<int64_t, int64_t> mateCol2Row ( Aeff.getcommgrid(), Aeff.getnrow(), (int64_t) -1);
-		//FullyDistVec<int64_t, int64_t> mateRight2Left ( Aeff.getcommgrid(), Aeff.getncol(), (int64_t) -1);
-        
-        
-		FullyDistVec<int64_t, int64_t> dvColVertices(Aeff.getcommgrid(), Aeff.getncol(), (int64_t) 0); // just for initialization of sparse vector
-        FullyDistVec<int64_t, int64_t> dvec;
-        dvec.iota(Aeff.getncol(), 0);
-		FullyDistSpVec<int64_t, int64_t> svColVertices(dvColVertices); // only works when svColVertices is empty
-        svColVertices.setNumToInd();
-        FullyDistSpVec<int64_t, int64_t> fringeRow(Aeff.getcommgrid(), Aeff.getnrow());
-        
-        FullyDistSpVec<int64_t, int64_t> unmatchedCol(dvColVertices);
-        
-        int64_t curUnmatchedCol = unmatchedCol.getnnz();
-        int64_t newlyMatched = 1; // ensure the first pass of the while loop
-        int iteration = 0;
-		tStart = MPI_Wtime();
-        MPI_Barrier(MPI_COMM_WORLD);
             
-        while(curUnmatchedCol !=0 && newlyMatched != 0 )
-        //while(iteration<1)
-        {
+            // find non isolated row and column vertices
+            FullyDistVec<int64_t, int64_t> nonisoRowV;	// id's of non-isolated (connected) Row vertices
+            FullyDistVec<int64_t, int64_t> nonisoColV;	// id's of non-isolated (connected) Col vertices
            
-            // step1: Find adjacent row vertices (col vertices parent, row vertices child)
-            fringeRow = SpMV(Aeff, unmatchedCol, optbuf);
+			FullyDistVec<int64_t, int64_t> * ColSums = new FullyDistVec<int64_t, int64_t>(Aeff.getcommgrid());
+			FullyDistVec<int64_t, int64_t> * RowSums = new FullyDistVec<int64_t, int64_t>(Aeff.getcommgrid());
+			Aeff.Reduce(*ColSums, Column, plus<int64_t>(), static_cast<int64_t>(0));
+			Aeff.Reduce(*RowSums, Row, plus<int64_t>(), static_cast<int64_t>(0));
+			
+            FullyDistSpVec<int64_t, int64_t> unmatchedRow(*RowSums, positive<int64_t>());
+            FullyDistSpVec<int64_t, int64_t> unmatchedCol(*ColSums, positive<int64_t>());
+            unmatchedRow.setNumToInd();
+            unmatchedCol.setNumToInd();
             
-            // step2: Remove matched row vertices
-            fringeRow = EWiseMult(fringeRow, mateRow2Col, true, (int64_t) -1);
+            delete ColSums;
+            delete RowSums;
+            
+            
+            //matching vector (dense)
+            FullyDistVec<int64_t, int64_t> mateRow2Col ( Aeff.getcommgrid(), Aeff.getnrow(), (int64_t) -1);
+            FullyDistVec<int64_t, int64_t> mateCol2Row ( Aeff.getcommgrid(), Aeff.getncol(), (int64_t) -1);
+            
+            //fringe vector (sparse)
+            FullyDistSpVec<int64_t, int64_t> fringeRow(Aeff.getcommgrid(), Aeff.getnrow());
+            FullyDistSpVec<int64_t, int64_t> fringeCol(Aeff.getcommgrid(), Aeff.getncol());
+            
+            //row and column indices... dummy vectors used for permutation. #todo: think better alternatives
+            FullyDistVec<int64_t, int64_t> dvColVertices;
+            dvColVertices.iota(Aeff.getncol(), 0);
+            FullyDistVec<int64_t, int64_t> dvRowVertices;
+            dvRowVertices.iota(Aeff.getnrow(), 0);
+            //FullyDistVec<int64_t, int64_t> dvColVertices(Aeff.getcommgrid(), Aeff.getncol(), (int64_t) 0); // just for
+            
+           
 
-            // step3: Remove duplicate row vertices
-            fringeRow = fringeRow.Uniq();
-            //fringeRow.DebugPrint();
+            MPI_Pcontrol(1,"BFS");
             
-            // step4: Update mateRow2Col with the newly matched row vertices
-            mateRow2Col.Set(fringeRow);
-            
-            // step5: Update mateCol2Row with the newly matched col vertices
-            auto temp = dvec(fringeRow);
-            //mateCol2Row.Set(dvColVertices(fringeRow)); // does mot work !!
-            mateCol2Row.Set(temp);
+            int64_t curUnmatchedCol = unmatchedCol.getnnz();
+            int64_t curUnmatchedRow = unmatchedRow.getnnz();
+            int64_t newlyMatched = 1; // ensure the first pass of the while loop
+            int iteration = 0;
+            tStart = MPI_Wtime();
+            MPI_Barrier(MPI_COMM_WORLD);
             
 
-            // step6: Update unmatchedCol by removing newly matched columns
-            unmatchedCol = EWiseMult(unmatchedCol, mateCol2Row, true, (int64_t) -1);
-
-            
-            // Print statistics
-            //MPI_Barrier(MPI_COMM_WORLD);
+            //dvRowVertices.DebugPrint();
+            while(curUnmatchedCol !=0 && curUnmatchedRow!=0 && newlyMatched != 0 )
+            {
+                
+                // step1: Find adjacent row vertices (col vertices parent, row vertices child)
+                //unmatchedCol.DebugPrint();
+                fringeRow = SpMV(Aeff, unmatchedCol, optbuf);
+                
+                // step2: Remove matched row vertices
+                fringeRow = EWiseMult(fringeRow, mateRow2Col, true, (int64_t) -1);
+                //fringeRow.DebugPrint();
+                
+                // step3: Remove duplicate row vertices
+                fringeRow = fringeRow.Uniq();
+                
+                
+                // step4: Update mateRow2Col with the newly matched row vertices
+                mateRow2Col.Set(fringeRow);
+                
+                // step5: Update mateCol2Row with the newly matched col vertices
+                auto temp = dvRowVertices(fringeRow);
+                //mateCol2Row.Set(dvColVertices(fringeRow)); // does not work !!
+                mateCol2Row.Set(temp);
+                
+                
+                // step6: Update unmatchedCol/unmatchedRow by removing newly matched columns/rows
+                unmatchedCol = EWiseMult(unmatchedCol, mateCol2Row, true, (int64_t) -1);
+                unmatchedRow = EWiseMult(unmatchedRow, mateRow2Col, true, (int64_t) -1);
+                
+                
+                ++iteration;
+                newlyMatched = fringeRow.getnnz();
+                if(myrank == 0)
+                {
+                    cout  << "Iteration: " << iteration << "  |  UMRow: " << curUnmatchedRow << "  |  UMCol: " << curUnmatchedCol << "  |  newlyMatched: " << newlyMatched << endl;
+                }
+                
+                curUnmatchedCol = unmatchedCol.getnnz();
+                curUnmatchedRow = unmatchedRow.getnnz();
+                MPI_Barrier(MPI_COMM_WORLD);
+                
+            }
+        
+        
+        
             //mateRow2Col.DebugPrint();
             //mateCol2Row.DebugPrint();
             
-            
-            ++iteration;
-            newlyMatched = fringeRow.getnnz();
-            if(myrank == 0)
+            //Check if this is a maximal matching
+ 
+            fringeRow = SpMV(Aeff, unmatchedCol, optbuf);
+            fringeRow = EWiseMult(fringeRow, mateRow2Col, true, (int64_t) -1);
+            if(fringeRow.getnnz() != 0)
             {
-                cout  << "Iteration: " << iteration << "  |  UMCol: " << curUnmatchedCol << "  |  newlyMatched: " << newlyMatched << endl;
+                if(myrank == 0)
+                    cout << "Not maximal matching!!\n";
             }
+            //Aeff.Transpose();
+            //fringeCol = SpMV(Aeff, unmatchedRow, optbuf);
+            //fringeCol = EWiseMult(fringeCol, mateCol2Row, true, (int64_t) -1);
             
-            curUnmatchedCol = unmatchedCol.getnnz();
-            MPI_Barrier(MPI_COMM_WORLD);
         
-        }
-            
-        sanityCheck(mateCol2Row, mateRow2Col, Aeff, optbuf);
     }
     
 		
-        
+
         
         
         /// stats
@@ -502,3 +589,72 @@ cout<< "debug\n";
 	return 0;
 }
 
+
+
+/*
+ * Check the validity of the matching solution
+ */
+/*
+template <class IT, class NT>
+void sanityCheck(FullyDistVec<IT,NT> & mateCol2Row, FullyDistVec<IT,NT> & mateRow2Col,
+                 SpParMat < int64_t, bool, SpDCCols<int32_t,bool> >& spAdjMat, OptBuf<int32_t, int64_t> & optbuf)
+{
+    //mateCol2Row.DebugPrint();
+    //mateRow2Col.DebugPrint();
+    int myrank;
+    MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+    
+    
+    // first check if this is a matching
+    for(int i=0; i< mateRow2Col.glen ; i++)
+    {
+        int t = mateRow2Col[i];
+        
+        if(t!=-1 && mateCol2Row[t]!=i)
+        {
+            if(myrank == 0)
+                cout << "Error!!\n";
+            break;
+        }
+    }
+    
+    for(int i=0; i< mateCol2Row.glen ; i++)
+    {
+        int t = mateCol2Row[i];
+        if(t!=-1 && mateRow2Col[t]!=i)
+        {
+            if(myrank == 0)
+                cout << "Error!!\n";
+            break;
+        }
+    }
+    
+    
+    // Check if this is a maximal matching
+    
+    FullyDistVec<IT, NT> dvColVertices(spAdjMat.getcommgrid(), spAdjMat.getncol(), (NT) 0);
+    FullyDistSpVec<IT, NT> unmatchedCol(dvColVertices);
+    unmatchedCol = EWiseMult(unmatchedCol, mateCol2Row, true, (int64_t) -1);
+    FullyDistSpVec<IT, NT> fringeRow(spAdjMat.getcommgrid(), spAdjMat.getnrow());
+    
+    fringeRow = SpMV(spAdjMat, unmatchedCol, optbuf);
+    fringeRow = EWiseMult(fringeRow, mateRow2Col, true, (int64_t) -1);
+    
+    
+    FullyDistVec<IT, NT> dvRowVertices(spAdjMat.getcommgrid(), spAdjMat.getnrow(), (NT) 0);
+    FullyDistSpVec<IT, NT> unmatchedRow(dvRowVertices);
+    unmatchedRow = EWiseMult(unmatchedRow, mateRow2Col, true, (int64_t) -1);
+    FullyDistSpVec<IT, NT> fringeCol(spAdjMat.getcommgrid(), spAdjMat.getncol());
+    
+    spAdjMat.Transpose();
+    fringeCol = SpMV(spAdjMat, unmatchedRow, optbuf);
+    fringeCol = EWiseMult(fringeCol, mateCol2Row, true, (int64_t) -1);
+    
+    if(fringeRow.getnnz() != 0 || fringeCol.getnnz() != 0)
+    {
+        if(myrank == 0)
+            cout << "Not maximam matching!!\n";
+    }
+    
+}
+*/
