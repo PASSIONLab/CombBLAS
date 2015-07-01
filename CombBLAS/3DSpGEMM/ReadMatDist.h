fd91ffb61ea81af541e1e319dea47df66a899620
aazad <aazad@localhost>
2015-07-01 23:50:09 +0000

    added reader

---
+++ CombBLAS/3DSpGEMM/ReadMatDist.h
@@ -0,0 +1,105 @@
+#ifndef _READ_MAT_DIST_H_
+#define _READ_MAT_DIST_H_
+
+#include <mpi.h>
+#include <sys/time.h> 
+#include <iostream>
+#include <iomanip>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+
+// These macros should be defined before stdint.h is included
+#ifndef __STDC_CONSTANT_MACROS
+#define __STDC_CONSTANT_MACROS
+#endif
+#ifndef __STDC_LIMIT_MACROS
+#define __STDC_LIMIT_MACROS
+#endif
+#include <stdint.h>
+
+#include "../CombBLAS.h"
+#include "Glue.h"   
+
+
+
+/**
+ ** \param[out] splitmat {generated RMAT matrix, split into CMG.GridLayers pieces}
+ **/
+template <typename IT, typename NT>
+void Reader(string filename, CCGrid & CMG, SpDCCols<IT,NT> & splitmat, bool trans)
+{
+    vector<IT> vecEss; // at layer_grid=0, this will have [CMG.GridLayers * SpDCCols<IT,NT>::esscount] entries
+    vector< SpDCCols<IT, NT> > partsmat;    // only valid at layer_grid=0
+    int nparts = CMG.GridLayers;
+	if(CMG.layer_grid == 0)
+	{
+		//SpDCCols<IT, NT> * localmat = GenRMat<IT,NT>(scale, EDGEFACTOR, initiator, CMG.layerWorld);
+        
+        SpParMat < IT, NT, SpDCCols<IT,NT> > *A;
+        A->ReadDistribute(filename, 0, false);
+        SpDCCols<IT, NT> * localmat = &A->seq();
+        
+        double trans_beg = MPI_Wtime();
+        if(trans) localmat->Transpose(); // locally transpose
+        comp_trans += (MPI_Wtime() - trans_beg);
+
+        double split_beg = MPI_Wtime();
+        localmat->ColSplit(nparts, partsmat);     // split matrices are emplaced-back into partsmat vector, localmat destroyed
+
+        for(int i=0; i< nparts; ++i)
+        {
+            vector<IT> ess = partsmat[i].GetEssentials();
+            for(auto itr = ess.begin(); itr != ess.end(); ++itr)
+            {
+                vecEss.push_back(*itr);
+            }
+        }
+        comp_split += (MPI_Wtime() - split_beg);
+	}
+    
+    double scatter_beg = MPI_Wtime();   // timer on
+    int esscnt = SpDCCols<IT,NT>::esscount; // necessary cast for MPI
+
+    vector<IT> myess(esscnt);
+    MPI_Scatter(vecEss.data(), esscnt, MPIType<IT>(), myess.data(), esscnt, MPIType<IT>(), 0, CMG.fiberWorld);
+    
+    if(CMG.layer_grid == 0) // senders
+    {
+        splitmat = partsmat[0]; // just copy the local split
+        for(int recipient=1; recipient< nparts; ++recipient)    // scatter the others
+        {
+            int tag = 0;
+            Arr<IT,NT> arrinfo = partsmat[recipient].GetArrays();
+            for(unsigned int i=0; i< arrinfo.indarrs.size(); ++i)	// get index arrays
+            {
+                // MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+                MPI_Send(arrinfo.indarrs[i].addr, arrinfo.indarrs[i].count, MPIType<IT>(), recipient, tag++, CMG.fiberWorld);
+            }
+            for(unsigned int i=0; i< arrinfo.numarrs.size(); ++i)	// get numerical arrays
+            {
+                MPI_Send(arrinfo.numarrs[i].addr, arrinfo.numarrs[i].count, MPIType<NT>(), recipient, tag++, CMG.fiberWorld);
+            }
+        }
+    }
+    else // receivers
+    {
+        splitmat.Create(myess);		// allocate memory for arrays
+        Arr<IT,NT> arrinfo = splitmat.GetArrays();
+
+        int tag = 0;
+        for(unsigned int i=0; i< arrinfo.indarrs.size(); ++i)	// get index arrays
+        {
+            MPI_Recv(arrinfo.indarrs[i].addr, arrinfo.indarrs[i].count, MPIType<IT>(), 0, tag++, CMG.fiberWorld, MPI_STATUS_IGNORE);
+        }
+        for(unsigned int i=0; i< arrinfo.numarrs.size(); ++i)	// get numerical arrays
+        {
+            MPI_Recv(arrinfo.numarrs[i].addr, arrinfo.numarrs[i].count, MPIType<NT>(), 0, tag++, CMG.fiberWorld, MPI_STATUS_IGNORE);
+        }
+    }
+    comm_split += (MPI_Wtime() - scatter_beg);
+}
+
+#endif
--- CombBLAS/3DSpGEMM/mpipspgemm.cpp
+++ CombBLAS/3DSpGEMM/mpipspgemm.cpp
@@ -14,6 +14,7 @@
 #include "Reductions.h"
 #include "Multiplier.h"
 #include "GenRmatDist.h"
+#include "ReadMatDist.h"
 
 using namespace std;
 
@@ -27,7 +28,7 @@ double comp_split;
 double comp_trans;
 double comm_split;
 
-#define ITERS 5
+#define ITERS 2
 
 int main(int argc, char *argv[])
 {
@@ -47,7 +48,7 @@ int main(int argc, char *argv[])
 	{
 		if(myrank == 0)
 		{
-			printf("Usage: ./mpipspgemm <Scale> <GridRows> <GridCols> <Replicas> <Type> <EDGEFACTOR> <algo>\n");
+			printf("Usage: ./mpipspgemm <Scale> <GridRows> <GridCols> <Layers> <Type> <EDGEFACTOR> <algo>\n");
 			printf("Example: ./mpipspgemm 19 4 4 2 ER 16 outer\n");
 			printf("Type ER: Erdos-Renyi\n");
 			printf("Type SSCA: R-MAT with SSCA benchmark parameters\n");
@@ -61,69 +62,91 @@ int main(int argc, char *argv[])
 	unsigned GRROWS = (unsigned) atoi(argv[2]);
 	unsigned GRCOLS = (unsigned) atoi(argv[3]);
 	unsigned C_FACTOR = (unsigned) atoi(argv[4]);
-	unsigned EDGEFACTOR = (unsigned) atoi(argv[6]);
-	double initiator[4];
-	
-	if(string(argv[5]) == string("ER"))
-	{
-		initiator[0] = .25;
-		initiator[1] = .25;
-		initiator[2] = .25;
-		initiator[3] = .25;
-	}
-	else if(string(argv[5]) == string("SSCA"))
-	{
-		initiator[0] = .57;
-		initiator[1] = .19;
-		initiator[2] = .19;
-		initiator[3] = .05;
-	}
-	else if(string(argv[5]) == string("G500"))
-	{
-		initiator[0] = .6;
-		initiator[1] = .4/3;
-		initiator[2] = .4/3;
-		initiator[3] = .4/3;
-	}
-	else {
-		if(myrank == 0)
-			printf("The initiator parameter - %s - is not recognized\n", argv[5]);
-	}
-
-
-	if(GRROWS != GRCOLS)
-	{
+    CCGrid CMG(C_FACTOR, GRCOLS);
+    
+    if(GRROWS != GRCOLS)
+    {
         SpParHelper::Print("This version of the Combinatorial BLAS only works on a square logical processor grid\n");
         MPI_Barrier(MPI_COMM_WORLD);
-		MPI_Abort(MPI_COMM_WORLD, 1);
-	}
-
-	int layer_length = GRROWS*GRCOLS;
-	if(layer_length * C_FACTOR != nprocs)
-	{
-		SpParHelper::Print("The product of <GridRows> <GridCols> <Replicas> does not match the number of processes\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    
+    int layer_length = GRROWS*GRCOLS;
+    if(layer_length * C_FACTOR != nprocs)
+    {
+        SpParHelper::Print("The product of <GridRows> <GridCols> <Replicas> does not match the number of processes\n");
         MPI_Barrier(MPI_COMM_WORLD);
-		MPI_Abort(MPI_COMM_WORLD, 1);
-	}
-
-    CCGrid CMG(C_FACTOR, GRCOLS);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    
+	
+	
     SpDCCols<int64_t, double> splitA, splitB;
-    Generator(scale, EDGEFACTOR, initiator, CMG, splitA, false);
-    Generator(scale, EDGEFACTOR, initiator, CMG, splitB, true); // also transpose before split
-    if(myrank == 0) printf("RMATs Generated and replicated along layers\n");
- 
-    if(string(argv[7]) == string("outer"))
+    string type;
+	
+    if(string(argv[5]) == string("input")) // input option
+    {
+        string fileA = string(argv[6]);
+        string fileB = string(argv[7]);
+        Reader(fileA, CMG, splitA, false);
+        Reader(fileB, CMG, splitB, true);
+        type = string(argv[8]);
+    }
+    else
+    {
+        
+        double initiator[4];
+        if(string(argv[5]) == string("ER"))
+        {
+            initiator[0] = .25;
+            initiator[1] = .25;
+            initiator[2] = .25;
+            initiator[3] = .25;
+        }
+        else if(string(argv[5]) == string("SSCA"))
+        {
+            initiator[0] = .57;
+            initiator[1] = .19;
+            initiator[2] = .19;
+            initiator[3] = .05;
+        }
+        else if(string(argv[5]) == string("G500"))
+        {
+            initiator[0] = .6;
+            initiator[1] = .4/3;
+            initiator[2] = .4/3;
+            initiator[3] = .4/3;
+        }
+        else {
+            if(myrank == 0)
+                printf("The initiator parameter - %s - is not recognized. Using default ER\n", argv[5]);
+             MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        
+        unsigned EDGEFACTOR = (unsigned) atoi(argv[6]);
+        Generator(scale, EDGEFACTOR, initiator, CMG, splitA, false);
+        Generator(scale, EDGEFACTOR, initiator, CMG, splitB, true); // also transpose before split
+        if(myrank == 0) printf("RMATs Generated and replicated along layers\n");
+        type = string(argv[7]);
+    }
+	
+	
+
+    
+    
+   
+    if(type == string("outer"))
     {
         for(int k=0; k<ITERS; k++)
             multiply_exp(splitA, splitB, CMG, true, false); // outer product
     }
-    else if(string(argv[7]) == string("column"))
+    else if(type == string("column"))
     {
         splitB.Transpose(); // locally "untranspose" [ABAB: check correctness]
         for(int k=0; k<ITERS; k++)
             multiply_exp(splitA, splitB, CMG, false, false);
     }
-    else if(string(argv[7]) == string("all"))
+    else if(type == string("all"))
     {
         for(int k=0; k<ITERS; k++)
             multiply_exp(splitA, splitB, CMG, true, false); // outer product
