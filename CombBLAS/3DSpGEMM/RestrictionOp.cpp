e1f405764b61e85d27510731480141a3e3ece5c1
Ariful Azad <Azad@Arifuls-MacBook-Pro.local>
2015-08-02 19:14:44 -0700

    SplitMatDist

---
+++ CombBLAS/3DSpGEMM/RestrictionOp.cpp
@@ -0,0 +1,183 @@
+#include <mpi.h>
+#include <sys/time.h>
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <stdint.h>
+#include <cmath>
+#include "../CombBLAS.h"
+#include "Glue.h"
+#include "CCGrid.h"
+#include "Reductions.h"
+#include "Multiplier.h"
+#include "SplitMatDist.h"
+#include "RestrictionOp.h"
+
+
+using namespace std;
+
+double comm_bcast;
+double comm_reduce;
+double comp_summa;
+double comp_reduce;
+double comp_result;
+double comp_reduce_layer;
+double comp_split;
+double comp_trans;
+double comm_split;
+
+#define ITERS 5 
+
+int main(int argc, char *argv[])
+{
+    int provided;
+	//MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
+    
+    
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &provided);
+    if (provided < MPI_THREAD_SERIALIZED)
+    {
+        printf("ERROR: The MPI library does not have MPI_THREAD_SERIALIZED support\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    
+    
+    
+    int nprocs, myrank;
+    MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+
+	if(argc < 8)
+	{
+		if(myrank == 0)
+		{
+			printf("Usage (random): ./mpipspgemm <GridRows> <GridCols> <Layers> <Type> <Scale> <EDGEFACTOR> <algo>\n");
+            printf("Usage (input): ./mpipspgemm <GridRows> <GridCols> <Layers> <Type=input> <matA> <matB> <algo>\n"); //TODO:<Scale>  not meaningful here. Need to remove it.  Still there because current scripts execute without error.
+			printf("Example: ./mpipspgemm 4 4 2 ER 19 16 outer\n");
+            printf("Example: ./mpipspgemm 4 4 2 Input matA.mtx matB.mtx threaded\n");
+			printf("Type ER: Erdos-Renyi\n");
+			printf("Type SSCA: R-MAT with SSCA benchmark parameters\n");
+			printf("Type G500: R-MAT with Graph500 benchmark parameters\n");
+            printf("algo: outer | column \n");
+		}
+		return -1;         
+	}
+
+	
+	unsigned GRROWS = (unsigned) atoi(argv[1]);
+	unsigned GRCOLS = (unsigned) atoi(argv[2]);
+	unsigned C_FACTOR = (unsigned) atoi(argv[3]);
+    CCGrid CMG(C_FACTOR, GRCOLS);
+    int nthreads;
+#pragma omp parallel
+    {
+        nthreads = omp_get_num_threads();
+    }
+
+    
+    if(GRROWS != GRCOLS)
+    {
+        SpParHelper::Print("This version of the Combinatorial BLAS only works on a square logical processor grid\n");
+        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    
+    int layer_length = GRROWS*GRCOLS;
+    if(layer_length * C_FACTOR != nprocs)
+    {
+        SpParHelper::Print("The product of <GridRows> <GridCols> <Replicas> does not match the number of processes\n");
+        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    
+	
+    {
+        SpDCCols<int32_t, double> splitA, splitB;
+        SpDCCols<int32_t, double> *splitC;
+        string type;
+        shared_ptr<CommGrid> layerGrid;
+        layerGrid.reset( new CommGrid(CMG.layerWorld, 0, 0) );
+        FullyDistVec<int32_t, int32_t> p(layerGrid); // permutation vector defined on layers
+        
+        if(string(argv[4]) == string("input")) // input option
+        {
+            string fileA(argv[5]);
+            string fileB(argv[6]);
+            
+            Reader(fileA, CMG, splitA, false, true, p); // p generated and used here
+            Reader(fileB, CMG, splitB, true, true, p); // p used here
+        }
+        else
+        {
+            
+            double initiator[4];
+            if(string(argv[4]) == string("ER"))
+            {
+                initiator[0] = .25;
+                initiator[1] = .25;
+                initiator[2] = .25;
+                initiator[3] = .25;
+            }
+            else if(string(argv[4]) == string("G500"))
+            {
+                initiator[0] = .57;
+                initiator[1] = .19;
+                initiator[2] = .19;
+                initiator[3] = .05;
+            }
+            else if(string(argv[4]) == string("SSCA"))
+            {
+                initiator[0] = .6;
+                initiator[1] = .4/3;
+                initiator[2] = .4/3;
+                initiator[3] = .4/3;
+            }
+            else {
+                if(myrank == 0)
+                    printf("The initiator parameter - %s - is not recognized. Using default ER\n", argv[5]);
+                MPI_Abort(MPI_COMM_WORLD, 1);
+            }
+            
+            unsigned scale = (unsigned) atoi(argv[5]);
+            unsigned EDGEFACTOR = (unsigned) atoi(argv[6]);
+            Generator(scale, EDGEFACTOR, initiator, CMG, splitA, false, true, p);
+            Generator(scale, EDGEFACTOR, initiator, CMG, splitB, true, true, p); // also transpose before split
+            if(myrank == 0) printf("RMATs Generated and replicated along layers\n");
+            
+        }
+        
+        
+        type = string(argv[7]);
+        if(myrank == 0)
+            printf("\n Processor Grid (row x col x layers x threads): %dx%dx%dx%d \n", CMG.GridRows, CMG.GridCols, CMG.GridLayers, nthreads);
+        
+        if(type == string("outer"))
+        {
+            for(int k=0; k<ITERS; k++)
+            {
+                splitC = multiply(splitA, splitB, CMG, true, false); // outer product
+                delete splitC;
+            }
+            
+        }
+        
+        else // default column-threaded
+        {
+            splitB.Transpose();
+            for(int k=0; k<ITERS; k++)
+            {
+                splitC = multiply(splitA, splitB, CMG, false, true);
+                delete splitC;
+            }
+            
+        }
+    }
+    
+	MPI_Finalize();
+	return 0;
+}
+	
+
---
+++ CombBLAS/3DSpGEMM/SplitMatDist.h
@@ -0,0 +1,198 @@
+#ifndef _GEN_RMAT_DIST_H_
+#define _GEN_RMAT_DIST_H_
+
+#include <mpi.h>
+#include <sys/time.h> 
+#include <iostream>
+#include <iomanip>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+
+// These macros should be defined before stdint.h is included
+#ifndef __STDC_CONSTANT_MACROS
+#define __STDC_CONSTANT_MACROS
+#endif
+#ifndef __STDC_LIMIT_MACROS
+#define __STDC_LIMIT_MACROS
+#endif
+#include <stdint.h>
+
+#include "../CombBLAS.h"
+#include "Glue.h"   
+
+
+template <typename IT, typename NT>
+SpDCCols<IT,NT> * ReadMat(string filename, CCGrid & CMG, SpDCCols<IT,NT> & splitmat, bool trans, bool permute, FullyDistVec<IT, IT>& p)
+{
+    double t01 = MPI_Wtime();
+    double t02;
+    if(CMG.layer_grid == 0)
+    {
+        shared_ptr<CommGrid> layerGrid;
+        layerGrid.reset( new CommGrid(CMG.layerWorld, 0, 0) );
+        SpParMat < IT, NT, SpDCCols<IT,NT> > *A = new SpParMat < IT, NT, SpDCCols<IT,NT> >(layerGrid);
+        A->ReadDistribute(filename, 0, false);
+        
+        SpParHelper::Print("Read input file\n");
+        ostringstream tinfo;
+        t02 = MPI_Wtime();
+        tinfo << "Reader took " << t02-t01 << " seconds" << endl;
+        SpParHelper::Print(tinfo.str());
+        
+        // random permutations for load balance
+        if(permute)
+        {
+            if(A->getnrow() == A->getncol())
+            {
+                if(p.TotalLength()!=A->getnrow())
+                {
+                    SpParHelper::Print("Generating random permutation vector.\n");
+                    p.iota(A->getnrow(), 0);
+                    p.RandPerm();
+                }
+                (*A)(p,p,true);// in-place permute to save memory
+            }
+            else
+            {
+                SpParHelper::Print("nrow != ncol. Can not apply symmetric permutation.\n");
+            }
+        }
+        
+        
+
+        double trans_beg = MPI_Wtime();
+        if(trans) A->Transpose(); // locally transpose
+        comp_trans += (MPI_Wtime() - trans_beg);
+        
+        float balance = A->LoadImbalance();
+        ostringstream outs;
+        outs << "Load balance: " << balance << endl;
+        SpParHelper::Print(outs.str());
+        
+        
+        return A->seqptr();
+
+    }
+    else
+        return new SpDCCols<IT,NT>();
+}
+
+template<typename IT, typename NT>
+SpDCCols<IT,NT> * GenMat(CCGrid & CMG, unsigned scale, unsigned EDGEFACTOR, double initiator[4], bool trans, bool scramble)
+{
+    double t01 = MPI_Wtime();
+    double t02;
+
+    if(CMG.layer_grid == 0)
+    {
+         shared_ptr<CommGrid> layerGrid;
+        layerGrid.reset( new CommGrid(CMG.layerWorld, 0, 0) );
+        
+        DistEdgeList<int64_t> * DEL = new DistEdgeList<int64_t>(layerworld);
+        
+        ostringstream minfo;
+        int nprocs = DEL->commGrid->GetSize();
+        minfo << "Started Generation of scale "<< scale << endl;
+        minfo << "Using " << nprocs << " MPI processes" << endl;
+        SpParHelper::Print(minfo.str());
+        
+        DEL->GenGraph500Data(initiator, scale, EDGEFACTOR, scramble, false );
+        // don't generate packed edges, that function uses MPI_COMM_WORLD which can not be used in a single layer!
+        
+        SpParHelper::Print("Generated renamed edge lists\n");
+        ostringstream tinfo;
+        t02 = MPI_Wtime();
+        tinfo << "Generation took " << t02-t01 << " seconds" << endl;
+        SpParHelper::Print(tinfo.str());
+        
+        SpParMat < IT, NT, SpDCCols<IT,NT> > *A = new SpParMat < IT, NT, SpDCCols<IT,NT> >(*DEL, false);
+        
+        delete DEL;
+        SpParHelper::Print("Created Sparse Matrix\n");
+        
+        double trans_beg = MPI_Wtime();
+        if(trans) A->Transpose(); // locally transpose
+        comp_trans += (MPI_Wtime() - trans_beg);
+        
+        float balance = A->LoadImbalance();
+        ostringstream outs;
+        outs << "Load balance: " << balance << endl;
+        SpParHelper::Print(outs.str());
+        
+        return A->seqptr();
+    }
+    else
+        return new SpDCCols<IT,NT>();
+}
+
+/**
+ ** \param[out] splitmat {split a matrix from layer 0 into CMG.GridLayers pieces}
+ **/
+template <typename IT, typename NT>
+void SplitMat(CCGrid & CMG, SpDCCols<IT, NT> * localmat, SpDCCols<IT,NT> & splitmat)
+{
+    vector<IT> vecEss; // at layer_grid=0, this will have [CMG.GridLayers * SpDCCols<IT,NT>::esscount] entries
+    vector< SpDCCols<IT, NT> > partsmat;    // only valid at layer_grid=0
+    int nparts = CMG.GridLayers;
+	if(CMG.layer_grid == 0)
+	{
+        double split_beg = MPI_Wtime();
+        localmat->ColSplit(nparts, partsmat);     // split matrices are emplaced-back into partsmat vector, localmat destroyed
+
+        for(int i=0; i< nparts; ++i)
+        {
+            vector<IT> ess = partsmat[i].GetEssentials();
+            for(auto itr = ess.begin(); itr != ess.end(); ++itr)
+            {
+                vecEss.push_back(*itr);
+            }
+        }
+        comp_split += (MPI_Wtime() - split_beg);
+	}
+    
+    double scatter_beg = MPI_Wtime();   // timer on
+    int esscnt = SpDCCols<IT,NT>::esscount; // necessary cast for MPI
+
+    vector<IT> myess(esscnt);
+    MPI_Scatter(vecEss.data(), esscnt, MPIType<IT>(), myess.data(), esscnt, MPIType<IT>(), 0, CMG.fiberWorld);
+    
+    if(CMG.layer_grid == 0) // senders
+    {
+        splitmat = partsmat[0]; // just copy the local split
+        for(int recipient=1; recipient< nparts; ++recipient)    // scatter the others
+        {
+            int tag = 0;
+            Arr<IT,NT> arrinfo = partsmat[recipient].GetArrays();
+            for(unsigned int i=0; i< arrinfo.indarrs.size(); ++i)	// get index arrays
+            {
+                // MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+                MPI_Send(arrinfo.indarrs[i].addr, arrinfo.indarrs[i].count, MPIType<IT>(), recipient, tag++, CMG.fiberWorld);
+            }
+            for(unsigned int i=0; i< arrinfo.numarrs.size(); ++i)	// get numerical arrays
+            {
+                MPI_Send(arrinfo.numarrs[i].addr, arrinfo.numarrs[i].count, MPIType<NT>(), recipient, tag++, CMG.fiberWorld);
+            }
+        }
+    }
+    else // receivers
+    {
+        splitmat.Create(myess);		// allocate memory for arrays
+        Arr<IT,NT> arrinfo = splitmat.GetArrays();
+
+        int tag = 0;
+        for(unsigned int i=0; i< arrinfo.indarrs.size(); ++i)	// get index arrays
+        {
+            MPI_Recv(arrinfo.indarrs[i].addr, arrinfo.indarrs[i].count, MPIType<IT>(), 0, tag++, CMG.fiberWorld, MPI_STATUS_IGNORE);
+        }
+        for(unsigned int i=0; i< arrinfo.numarrs.size(); ++i)	// get numerical arrays
+        {
+            MPI_Recv(arrinfo.numarrs[i].addr, arrinfo.numarrs[i].count, MPIType<NT>(), 0, tag++, CMG.fiberWorld, MPI_STATUS_IGNORE);
+        }
+    }
+    comm_split += (MPI_Wtime() - scatter_beg);
+}
+
+#endif
