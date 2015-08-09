0ce3b315b1bee80849e621f538cb24c3fd4f70b6
Ariful Azad <azad@titan-ext7.ccs.ornl.gov>
2015-08-09 02:40:38 -0400

    titan results G500

---
+++ CombBLAS/3DSpGEMM/titan_exp/file_process.sh
@@ -0,0 +1,11 @@
+#pass foldername as argument
+search_dir=$1
+for entry in "$search_dir"/*
+do
+  echo "$entry"
+  sed -n '/prow pcol/{n;p;p;p;p;p}' $entry >> $entry.txt
+done
+
+
+
+#sed -n '/prow pcol/{n;p;p;p;p;p}' spGEMMexp_G500_27_65536.o2441622
