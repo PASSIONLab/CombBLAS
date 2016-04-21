74a46d57339be71eb7b8273fa078de5e9a0fc527
aydozz <aydinbuluc@gmail.com>
2016-04-21 07:53:58 -0700

    mcl compiles

--- CombBLAS/ParFriends.h
+++ CombBLAS/ParFriends.h
@@ -180,7 +180,8 @@ bool CheckSpGEMMCompliance(const MATRIXA & A, const MATRIXB & B)
 /**
  * Broadcasts A multiple times (#phases) in order to save storage in the output
  * Only uses 1/phases of C memory if the threshold/max limits are proper
- **/
+ ** ABAB: Incomplete as of April 21
+ *
 template <typename SR, typename NUO, typename UDERO, typename IU, typename NU1, typename NU2, typename UDERA, typename UDERB>
 SpParMat<IU,NUO,UDERO> MemEfficientSpGEMM (SpParMat<IU,NU1,UDERA> & A, SpParMat<IU,NU2,UDERB> & B,
                                            int phases, NT0 hardthreshold)
@@ -274,7 +275,7 @@ SpParMat<IU,NUO,UDERO> MemEfficientSpGEMM (SpParMat<IU,NU1,UDERA> & A, SpParMat<
     // First get the result in SpTuples, then convert to UDER
     return SpParMat<IU,NUO,UDERO> (C, GridC);		// return the result object
 }
-
+*/
 
 
 /**
