4fab2fa7c1c1f65ef098f60d66cff4ada8677897
aydozz <aydinbuluc@gmail.com>
2016-04-21 10:56:20 -0700

    ColConcatenate implemented/tested

--- CombBLAS/SpDCCols.h
+++ CombBLAS/SpDCCols.h
@@ -1,11 +1,11 @@
 /****************************************************************/
 /* Parallel Combinatorial BLAS Library (for Graph Computations) */
-/* version 1.5 -------------------------------------------------*/
-/* date: 10/09/2015 ---------------------------------------------*/
+/* version 1.6 -------------------------------------------------*/
+/* date: 05/15/2016 --------------------------------------------*/
 /* authors: Ariful Azad, Aydin Buluc, Adam Lugowski ------------*/
 /****************************************************************/
 /*
- Copyright (c) 2010-2015, The Regents of the University of California
+ Copyright (c) 2010-2016, The Regents of the University of California
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
@@ -251,6 +251,8 @@ public:
 	}
     
     void ColSplit(int parts, vector< SpDCCols<IT,NT> > & matrices); //!< \attention Destroys calling object (*this)
+    void ColConcatenate(vector< SpDCCols<IT,NT> > & matrices);    //!< \attention Destroys its parameters (matrices)
+
 	void Split(SpDCCols<IT,NT> & partA, SpDCCols<IT,NT> & partB); 	//!< \attention Destroys calling object (*this)
 	void Merge(SpDCCols<IT,NT> & partA, SpDCCols<IT,NT> & partB);	//!< \attention Destroys its parameters (partA & partB)
 
