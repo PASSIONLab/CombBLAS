d3d68dbfca7b43eb8dd33bb4fd30de472eeaaca1
Ariful Azad <Azad@arifuls-mbp.dhcp.lbnl.us>
2016-07-22 13:40:17 -0700

    using multithreaded MultiwayMerge and LocalSpGEMM in MemEfficientSpGEMM

---
+++ CombBLAS/MultiwayMerge.h
@@ -0,0 +1,217 @@
+#include "CombBLAS.h"
+
+
+/***************************************************************************
+ * Find indices of column splitters in a list of tuple in parallel.
+ * Inputs:
+ *      tuples: an array of SpTuples each tuple is (rowid, colid, val)
+ *      nsplits: number of splits requested
+ *  Output:
+ *      splitters: An array of size (nsplits+1) storing the starts and ends of split tuples.
+ *      different type used for output since we might need int or IT
+ ***************************************************************************/
+
+template <typename RT, typename IT, typename NT>
+vector<RT> findColSplitters(SpTuples<IT,NT> * & spTuples, int nsplits)
+{
+    vector<RT> splitters(nsplits+1);
+    splitters[0] = static_cast<RT>(0);
+    ColLexiCompare<IT,NT> comp;
+#pragma omp parallel for
+    for(int i=1; i< nsplits; i++)
+    {
+        IT cur_col = i * (spTuples->getncol()/nsplits);
+        tuple<IT,IT,NT> search_tuple(0, cur_col, 0);
+        tuple<IT,IT,NT>* it = lower_bound (spTuples->tuples, spTuples->tuples + spTuples->getnnz(), search_tuple, comp);
+        splitters[i] = (RT) (it - spTuples->tuples);
+    }
+    splitters[nsplits] = spTuples->getnnz();
+    
+    return splitters;
+}
+
+
+
+
+/*
+ "Internal function" called by MultiwayMerge inside threaded region.
+ Never called from outside.
+ Assumption1: the input lists are already column sorted
+ Assumption2: at least two lists are passed to this function
+ Assumption3: the input and output lists are to be deleted by caller
+ */
+
+template<class SR, class IT, class NT>
+SpTuples<IT,NT>* SerialMerge( const vector<SpTuples<IT,NT> *> & ArrSpTups, tuple<IT, IT, NT> * ntuples)
+{
+    int nlists =  ArrSpTups.size();
+    ColLexiCompare<IT,int> heapcomp;
+    vector<tuple<IT, IT, int>> heap(nlists); // if performance issue, create this outside of threaded region
+    vector<IT> curptr(nlists, static_cast<IT>(0));
+    IT estnnz = 0;
+    IT hsize = 0;
+    for(int i=0; i< nlists; ++i)
+    {
+        if(ArrSpTups[i]->getnnz()>0)
+        {
+            estnnz += ArrSpTups[i]->getnnz();
+            heap[hsize++] = make_tuple(get<0>(ArrSpTups[i]->tuples[0]), get<1>(ArrSpTups[i]->tuples[0]), i);
+        }
+        
+    }
+    make_heap(heap.data(), heap.data()+hsize, not2(heapcomp));
+    IT cnz = 0;
+    
+    while(hsize > 0)
+    {
+        pop_heap(heap.data(), heap.data() + hsize, not2(heapcomp));   // result is stored in heap[hsize-1]
+        int source = get<2>(heap[hsize-1]);
+        
+        if( (cnz != 0) &&
+           ((get<0>(ntuples[cnz-1]) == get<0>(heap[hsize-1])) && (get<1>(ntuples[cnz-1]) == get<1>(heap[hsize-1]))) )
+        {
+            get<2>(ntuples[cnz-1])  = SR::add(get<2>(ntuples[cnz-1]), ArrSpTups[source]->numvalue(curptr[source]++));
+        }
+        else
+        {
+            ntuples[cnz++] = ArrSpTups[source]->tuples[curptr[source]++];
+        }
+        
+        if(curptr[source] != ArrSpTups[source]->getnnz())	// That array has not been depleted
+        {
+            heap[hsize-1] = make_tuple(get<0>(ArrSpTups[source]->tuples[curptr[source]]),
+                                       get<1>(ArrSpTups[source]->tuples[curptr[source]]), source);
+            push_heap(heap.data(), heap.data()+hsize, not2(heapcomp));
+        }
+        else
+        {
+            --hsize;
+        }
+    }
+    return new SpTuples<IT,NT> (cnz, ArrSpTups[0]->getnrow(), ArrSpTups[0]->getncol(), ntuples, true);
+
+}
+
+
+
+// Performs a balanced merge of the array of SpTuples
+// Assumes the input parameters are already column sorted
+template<class SR, class IT, class NT>
+SpTuples<IT, NT>* MultiwayMerge( vector<SpTuples<IT,NT> *> & ArrSpTups, IT mdim = 0, IT ndim = 0, bool delarrs = false )
+{
+    
+    int nlists =  ArrSpTups.size();
+    if(nlists == 0)
+    {
+        return new SpTuples<IT,NT>(0, mdim, ndim); //empty mxn SpTuples
+    }
+    if(nlists == 1)
+    {
+        if(delarrs) // steal data from input, and don't delete input
+        {
+            return ArrSpTups[0];
+        }
+        else // copy input to output
+        {
+            tuple<IT, IT, NT>* mergeTups = static_cast<tuple<IT, IT, NT>*>
+                    (::operator new (sizeof(tuple<IT, IT, NT>[ArrSpTups[0]->getnnz()])));
+            #pragma omp parallel for
+            for(int i=0; i<ArrSpTups[0]->getnnz(); i++)
+                mergeTups[i] = ArrSpTups[0]->tuples[i];
+
+            return new SpTuples<IT,NT> (ArrSpTups[0]->getnnz(), mdim, ndim, mergeTups, true);
+        }
+    }
+    
+    // ---- check correctness of input dimensions ------
+    for(int i=0; i< nlists; ++i)
+    {
+        if((mdim != ArrSpTups[i]->getnrow()) || ndim != ArrSpTups[i]->getncol())
+        {
+            cerr << "Dimensions of SpTuples do not match on multiwayMerge()" << endl;
+            return new SpTuples<IT,NT>(0,0,0);
+        }
+    }
+
+    int nthreads;
+#pragma omp parallel
+    {
+        nthreads = omp_get_num_threads();
+    }
+    int nsplits = 4*nthreads; // oversplit for load balance
+    nsplits = min(nsplits, (int)ndim); // we cannot split a column
+    vector< vector<IT> > colPtrs;
+    for(int i=0; i< nlists; i++)
+    {
+        colPtrs.push_back(findColSplitters<IT>(ArrSpTups[i], nsplits)); // in parallel
+    }
+    
+    // ------ estimate memory requirement after merge in each split ------
+    vector<IT> nnzPerSplit(nsplits);
+    IT nnzAll = static_cast<IT>(0);
+    //#pragma omp parallel for
+    for(int i=0; i< nsplits; i++)
+    {
+        IT t = static_cast<IT>(0);
+        for(int j=0; j< nlists; ++j)
+            t += colPtrs[j][i+1] - colPtrs[j][i];
+        nnzPerSplit[i] = t;
+        nnzAll += t;
+    }
+
+    
+    
+    // ------ allocate memory in a serial region ------
+    vector<tuple<IT, IT, NT> *> mergeBuf(nsplits);
+    for(int i=0; i< nsplits; i++)
+    {
+        mergeBuf[i] = static_cast<tuple<IT, IT, NT>*> (::operator new (sizeof(tuple<IT, IT, NT>[nnzPerSplit[i]])));
+    }
+
+
+     // ------ perform merge in parallel ------
+    vector<SpTuples<IT,NT> *> listMergeTups(nsplits); // use the memory allocated in mergeBuf
+#pragma omp parallel for schedule(dynamic)
+    for(int i=0; i< nsplits; i++) // serially merge part by part
+    {
+        vector<SpTuples<IT,NT> *> listSplitTups(nlists);
+        for(int j=0; j< nlists; ++j)
+        {
+            IT curnnz= colPtrs[j][i+1] - colPtrs[j][i];
+            listSplitTups[j] = new SpTuples<IT, NT> (curnnz, mdim, ndim, ArrSpTups[j]->tuples + colPtrs[j][i], true);
+        }
+        listMergeTups[i] = SerialMerge<SR>(listSplitTups, mergeBuf[i]);
+    }
+    
+    
+    // ------ concatenate merged tuples processed by threads ------
+    vector<IT> tdisp(nsplits+1);
+    tdisp[0] = 0;
+    for(int i=0; i<nsplits; ++i)
+    {
+        tdisp[i+1] = tdisp[i] + listMergeTups[i]->getnnz();
+    }
+
+    IT mergedListSize = tdisp[nsplits];
+    tuple<IT, IT, NT>* shrunkTuples = static_cast<tuple<IT, IT, NT>*> (::operator new (sizeof(tuple<IT, IT, NT>[mergedListSize])));
+    
+#pragma omp parallel for schedule(dynamic)
+    for(int i=0; i< nsplits; i++)
+    {
+        std::copy(listMergeTups[i]->tuples , listMergeTups[i]->tuples + listMergeTups[i]->getnnz(), shrunkTuples + tdisp[i]);
+    }
+
+    
+    for(int i=0; i< nsplits; i++)
+    {
+        //::operator delete(listMergeTups[i]->tuples);
+        ::operator delete(mergeBuf[i]);
+    }
+    
+    for(int i=0; i< nlists; i++)
+    {
+        if(delarrs)
+            delete ArrSpTups[i]; // this might be expensive for large local matrices
+    }
+    return new SpTuples<IT, NT> (mergedListSize, mdim, ndim, shrunkTuples, true);
+}
---
+++ CombBLAS/mtSpGEMM.h
@@ -0,0 +1,292 @@
+#ifndef _mtSpGEMM_h
+#define _mtSpGEMM_h
+
+#include "CombBLAS.h"
+
+/*
+ Multithreaded prefix sum
+ Inputs:
+    in: an input array
+    size: the length of the input array "in"
+    nthreads: number of threads used to compute the prefix sum
+ 
+ Output:
+    return an array of size "size+1"
+    the memory of the output array is allocated internallay
+ 
+ Example:
+ 
+    in = [2, 1, 3, 5]
+    out = [0, 2, 3, 6, 11]
+ */
+template <typename T>
+T* prefixsum(T* in, int size, int nthreads)
+{
+    vector<T> tsum(nthreads+1);
+    tsum[0] = 0;
+    T* out = new T[size+1];
+    out[0] = 0;
+    T* psum = &out[1];
+    
+#pragma omp parallel
+    {
+        int ithread = omp_get_thread_num();
+        T sum = 0;
+#pragma omp for schedule(static)
+        for (int i=0; i<size; i++)
+        {
+            sum += in[i];
+            psum[i] = sum;
+        }
+        
+        tsum[ithread+1] = sum;
+#pragma omp barrier
+        T offset = 0;
+        for(int i=0; i<(ithread+1); i++)
+        {
+            offset += tsum[i];
+        }
+#pragma omp for schedule(static)
+        for (int i=0; i<size; i++)
+        {
+            psum[i] += offset;
+        }
+    
+    }
+    return out;
+}
+
+
+
+
+// multithreaded HeapSpGEMM
+template <typename SR, typename NTO, typename IT, typename NT1, typename NT2>
+SpTuples<IT, NTO> * LocalSpGEMM
+(const SpDCCols<IT, NT1> & A,
+ const SpDCCols<IT, NT2> & B,
+ bool clearA, bool clearB)
+{
+    IT mdim = A.getnrow();
+    IT ndim = B.getncol();
+    IT nnzA = A.getnnz();
+    if(A.isZero() || B.isZero())
+    {
+        return new SpTuples<IT, NTO>(0, mdim, ndim);
+    }
+    
+    Dcsc<IT,NT1>* Adcsc = A.GetDCSC();
+    Dcsc<IT,NT2>* Bdcsc = B.GetDCSC();
+    IT nA = A.getncol();
+    IT cnzmax = Adcsc->nz + Bdcsc->nz;	// estimate on the size of resulting matrix C
+    float cf  = static_cast<float>(nA+1) / static_cast<float>(Adcsc->nzc);
+    IT csize = static_cast<IT>(ceil(cf));   // chunk size
+    IT * aux;
+    Adcsc->ConstructAux(nA, aux);
+    
+    int numThreads;
+#pragma omp parallel
+    {
+        numThreads = omp_get_num_threads();
+    }
+   
+    IT* colnnzC = estimateNNZ(A, B);
+    IT* colptrC = prefixsum<IT>(colnnzC, Bdcsc->nzc, numThreads);
+    delete [] colnnzC;
+    IT nnzc = colptrC[Bdcsc->nzc];
+    tuple<IT,IT,NTO> * tuplesC = static_cast<tuple<IT,IT,NTO> *> (::operator new (sizeof(tuple<IT,IT,NTO>[nnzc])));
+    
+    // thread private space for heap and colinds
+    vector<vector< pair<IT,IT>>> colindsVec(numThreads);
+    vector<vector<HeapEntry<IT,NT1>>> globalheapVec(numThreads);
+    
+    for(int i=0; i<numThreads; i++) //inital allocation per thread, may be an overestimate, but does not require more memoty than inputs
+    {
+        colindsVec[i].resize(nnzA/numThreads);
+        globalheapVec[i].resize(nnzA/numThreads);
+    }
+    
+    
+#pragma omp parallel for
+    for(int i=0; i < Bdcsc->nzc; ++i)
+    {
+        IT nnzcolB = Bdcsc->cp[i+1] - Bdcsc->cp[i]; //nnz in the current column of B
+        int myThread = omp_get_thread_num();
+        if(colindsVec[myThread].size() < nnzcolB) //resize thread private vectors if needed
+        {
+            colindsVec[myThread].resize(nnzcolB);
+            globalheapVec[myThread].resize(nnzcolB);
+        }
+        
+        
+        // colinds.first vector keeps indices to A.cp, i.e. it dereferences "colnums" vector (above),
+        // colinds.second vector keeps the end indices (i.e. it gives the index to the last valid element of A.cpnack)
+        Adcsc->FillColInds(Bdcsc->ir + Bdcsc->cp[i], nnzcolB, colindsVec[myThread], aux, csize);
+        pair<IT,IT> * colinds = colindsVec[myThread].data();
+        HeapEntry<IT,NT1> * wset = globalheapVec[myThread].data();
+        IT hsize = 0;
+        
+        
+        for(IT j = 0; (unsigned)j < nnzcolB; ++j)		// create the initial heap
+        {
+            if(colinds[j].first != colinds[j].second)	// current != end
+            {
+                wset[hsize++] = HeapEntry< IT,NT1 > (Adcsc->ir[colinds[j].first], j, Adcsc->numx[colinds[j].first]);
+            }
+        }
+        make_heap(wset, wset+hsize);
+        
+        IT curptr = colptrC[i];
+        while(hsize > 0)
+        {
+            pop_heap(wset, wset + hsize);         // result is stored in wset[hsize-1]
+            IT locb = wset[hsize-1].runr;	// relative location of the nonzero in B's current column
+            
+            NTO mrhs = SR::multiply(wset[hsize-1].num, Bdcsc->numx[Bdcsc->cp[i]+locb]);
+            if (!SR::returnedSAID())
+            {
+                if( (curptr > colptrC[i]) && get<0>(tuplesC[curptr-1]) == wset[hsize-1].key)
+                {
+                    get<2>(tuplesC[curptr-1]) = SR::add(get<2>(tuplesC[curptr-1]), mrhs);
+                }
+                else
+                {
+                    tuplesC[curptr++]= make_tuple(wset[hsize-1].key, Bdcsc->jc[i], mrhs) ;
+                }
+                
+            }
+            
+            if( (++(colinds[locb].first)) != colinds[locb].second)	// current != end
+            {
+                // runr stays the same !
+                wset[hsize-1].key = Adcsc->ir[colinds[locb].first];
+                wset[hsize-1].num = Adcsc->numx[colinds[locb].first];
+                push_heap(wset, wset+hsize);
+            }
+            else
+            {
+                --hsize;
+            }
+        }
+    }
+    
+    if(clearA)
+        delete const_cast<SpDCCols<IT, NT1> *>(&A);
+    if(clearB)
+        delete const_cast<SpDCCols<IT, NT2> *>(&B);
+    
+    delete [] colptrC;
+    delete [] aux;
+    
+    SpTuples<IT, NTO>* spTuplesC = new SpTuples<IT, NTO> (nnzc, mdim, ndim, tuplesC, true);
+    return spTuplesC;
+    
+}
+
+// estimate space for result of SpGEMM
+template <typename IT, typename NT1, typename NT2>
+IT* estimateNNZ(const SpDCCols<IT, NT1> & A,const SpDCCols<IT, NT2> & B)
+{
+    IT nnzA = A.getnnz();
+    if(A.isZero() || B.isZero())
+    {
+        return NULL;
+    }
+    
+    double tstart = MPI_Wtime();
+    Dcsc<IT,NT1>* Adcsc = A.GetDCSC();
+    Dcsc<IT,NT2>* Bdcsc = B.GetDCSC();
+    
+    float cf  = static_cast<float>(A.getncol()+1) / static_cast<float>(Adcsc->nzc);
+    IT csize = static_cast<IT>(ceil(cf));   // chunk size
+    IT * aux;
+    Adcsc->ConstructAux(A.getncol(), aux);
+    
+    
+    int numThreads;
+#pragma omp parallel
+    {
+        numThreads = omp_get_num_threads();
+    }
+    
+
+    IT* colnnzC = new IT[Bdcsc->nzc]; // nnz in every nonempty column of C
+#pragma omp parallel for
+    for(IT i=0; i< Bdcsc->nzc; ++i)
+    {
+        colnnzC[i] = 0;
+    }
+    
+    // thread private space for heap and colinds
+    vector<vector< pair<IT,IT>>> colindsVec(numThreads);
+    vector<vector<pair<IT,IT>>> globalheapVec(numThreads);
+    
+    double tmemStart = MPI_Wtime();
+    for(int i=0; i<numThreads; i++) //inital allocation per thread, may be an overestimate, but does not require more memoty than inputs
+    {
+        colindsVec[i].resize(nnzA/numThreads);
+        globalheapVec[i].resize(nnzA/numThreads);
+    }
+    double tmem = MPI_Wtime() - tmemStart;
+    
+#pragma omp parallel for
+    for(int i=0; i < Bdcsc->nzc; ++i)
+    {
+        IT nnzcolB = Bdcsc->cp[i+1] - Bdcsc->cp[i]; //nnz in the current column of B
+        int myThread = omp_get_thread_num();
+        if(colindsVec[myThread].size() < nnzcolB) //resize thread private vectors if needed
+        {
+            tmemStart = MPI_Wtime();
+            colindsVec[myThread].resize(nnzcolB);
+            globalheapVec[myThread].resize(nnzcolB);
+            tmem += (MPI_Wtime() - tmemStart);
+        }
+        
+
+        // colinds.first vector keeps indices to A.cp, i.e. it dereferences "colnums" vector (above),
+        // colinds.second vector keeps the end indices (i.e. it gives the index to the last valid element of A.cpnack)
+        Adcsc->FillColInds(Bdcsc->ir + Bdcsc->cp[i], nnzcolB, colindsVec[myThread], aux, csize);
+        pair<IT,IT> * colinds = colindsVec[myThread].data();
+        pair<IT,IT> * curheap = globalheapVec[myThread].data();
+        IT hsize = 0;
+        
+        // create the initial heap
+        for(IT j = 0; (unsigned)j < nnzcolB; ++j)
+        {
+            if(colinds[j].first != colinds[j].second)
+            {
+                curheap[hsize++] = make_pair(Adcsc->ir[colinds[j].first], j);
+            }
+        }
+        make_heap(curheap, curheap+hsize, greater<pair<IT,IT>>());
+        
+        IT prevRow=-1; // previously popped row from heap
+        
+        while(hsize > 0)
+        {
+            pop_heap(curheap, curheap + hsize, greater<pair<IT,IT>>()); // result is stored in wset[hsize-1]
+            IT locb = curheap[hsize-1].second;
+            
+            if( curheap[hsize-1].first != prevRow)
+            {
+                prevRow = curheap[hsize-1].first;
+                colnnzC[i] ++;
+            }
+            
+            if( (++(colinds[locb].first)) != colinds[locb].second)	// current != end
+            {
+                curheap[hsize-1].first = Adcsc->ir[colinds[locb].first];
+                push_heap(curheap, curheap+hsize, greater<pair<IT,IT>>());
+            }
+            else
+            {
+                --hsize;
+            }
+        }
+    }
+    
+    delete [] aux;
+    return colnnzC;
+}
+
+
+#endif
