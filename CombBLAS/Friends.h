ddec1cac09903aabf490536c305907452ce760d7
Ariful Azad <Azad@Arifuls-MacBook-Pro.local>
2016-10-20 14:06:10 -0700

    preallocated-SPA based DCSC

--- CombBLAS/Friends.h
+++ CombBLAS/Friends.h
@@ -111,7 +111,6 @@ int generic_gespmv_threaded (const SpMat<IU,NUM,DER> & A, const int32_t * indx, 
 			{
                 if(SPA.initialized)
                 {
-                    cout << "using this" << endl;
                     if(i != splits-1)
                          SpMXSpV_ForThreading<SR>(*(A.GetInternal(i)), perpiece, indx, numx, nnzx, indy[i], numy[i], i*perpiece, SPA.V_localy[i], SPA.V_isthere[i], SPA.V_inds[i]);
                     else
