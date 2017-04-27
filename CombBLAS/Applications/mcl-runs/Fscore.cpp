0c1012969ebd48557694bfae358869a62f81e767
Ariful Azad <Azad@17.215.128.198.in-addr.dhcp.lbnl.us>
2017-04-26 17:35:32 -0700

    updated F score calculation and outputing mismatches

--- CombBLAS/Applications/mcl-runs/Fscore.cpp
+++ CombBLAS/Applications/mcl-runs/Fscore.cpp
@@ -23,119 +23,119 @@ vector<size_t> sortIndices(const vector<T> &v) {
     return idx;
 }
 
-int64_t intersect(vector<int64_t> vec1, vector<int64_t> vec2)
-{
-    int64_t isect = 0;
-    for(int i=0, j=0; i<vec1.size() && j<vec2.size();)
-    {
-        if(vec1[i] < vec2[j]) i++;
-        else if(vec2[j] < vec1[i]) j++;
-        else
-        {
-            isect ++;
-            i++; j++;
-        }
-    }
-    return isect;
-}
 
-// fname1 is the reference (form MCL)
-double Fscore(string fname1, string fname2, int base)
+
+double Fscore(string mclFile, string hipmclFile, int base)
 {
-    ifstream infile1 (fname1);
-    ifstream infile2 (fname2);
+    ifstream mclStream (mclFile);
+    ifstream hipmclStream (hipmclFile);
     string line;
     int64_t item, clustID = 0;
-    int64_t nclust1, nclust2;
-    int64_t N1 = 0, N2 = 0; // total items
+    int64_t numMCLClusters, numHipMCLClusters;
+    int64_t nproteins = 0;
     // item 0-based
     
-    vector<vector<int64_t>> clusters1;
+    vector<vector<int64_t>> mclClusters;
     
-    if (infile1.is_open())
+    if (mclStream.is_open())
     {
-        while ( getline (infile1,line) )
+        while ( getline (mclStream,line) )
         {
             istringstream iss(line);
-            if(clustID >= clusters1.size())
+            if(clustID >= mclClusters.size())
             {
-                clusters1.resize(clustID * 2 + 1);
+                mclClusters.resize(clustID * 2 + 1);
             }
             while ( iss >> item)
             {
                 if(base==1) item --;
-                clusters1[clustID].push_back(item);
+                mclClusters[clustID].push_back(item);
             }
             
-            N1 += clusters1[clustID].size();
+            nproteins += mclClusters[clustID].size();
             clustID++;
         }
-        infile1.close();
-        nclust1 = clustID;
+        mclStream.close();
+        numMCLClusters = clustID;
     }
     else
     {
-        cout << "Unable to open " << fname1 << endl;
+        cout << "Unable to open " << mclFile << endl;
         return -1;
     }
     
-    vector<int64_t> clust2(N1, -1);
+    cout << "Number of clusters from MCL: " << numMCLClusters << endl;
+    vector<int64_t> clust2(nproteins, -1);
     clustID = 0;
-    if (infile2.is_open())
+    if (hipmclStream.is_open())
     {
-        while ( getline (infile2,line) )
+        while ( getline (hipmclStream,line) )
         {
             istringstream iss(line);
             while ( iss >> item)
             {
                 if(base==1) item --;
                 clust2[item] = clustID;
+                if(item >= nproteins)
+                {
+                    cout << "The number of vertices in MCL and HipMCL outputs does not match. \n Please check if there are isolated vertices in HipMCL.\n Exiting.............." << endl;
+                    exit(1);
+                }
             }
             clustID++;
         }
-        infile2.close();
-        nclust2 = clustID;
+        hipmclStream.close();
+        numHipMCLClusters = clustID;
     }
     else
     {
-        cout << "Unable to open " << fname2 << endl;
+        cout << "Unable to open " << hipmclFile << endl;
         return -1;
     }
     
-    vector<int64_t> clusterSizes2(nclust2,0);
-    for(int i=0; i<N1; i++)
+    cout << "Number of clusters from HipMCL: " << numHipMCLClusters << endl;
+    
+    vector<int64_t> clusterSizes2(numHipMCLClusters,0);
+    for(int i=0; i<nproteins; i++)
     {
         clusterSizes2[clust2[i]]++;
     }
     
     
-    vector<double> F(nclust1);
-    vector<int64_t> nisect(nclust2); // number of items in the intersection
-    vector<int64_t> isect(nclust2); // clusters with nonzero intersection with the current cluster
-    
+    vector<double> F(numMCLClusters);
+    vector<int64_t> nisect(numHipMCLClusters); // number of items in the intersection
+    vector<int64_t> isect(numHipMCLClusters); // clusters with nonzero intersection with the current cluster
+    int mismatch = 0;
 
-    for(int i=0; i<nclust1; i++)
+    for(int i=0; i<numMCLClusters; i++)
     {
         int64_t isectCount = 0;
         fill(nisect.begin(), nisect.end(), 0);
-        for(int j=0; j<clusters1[i].size(); j++)
+        for(int j=0; j<mclClusters[i].size(); j++)
         {
-            int64_t item1 = clusters1[i][j];
+            int64_t item1 = mclClusters[i][j];
             int64_t c2 = clust2[item1];
             if(nisect[c2]==0) isect[isectCount++] = c2;
             nisect[c2] ++;
+        }
+        auto maxoverlap = max_element(nisect.begin(), nisect.end());
+        int64_t c2_max = distance(nisect.begin(), maxoverlap);
+        if(*maxoverlap!=mclClusters[i].size() || *maxoverlap!=clusterSizes2[c2_max])
+        {
+            cout << "Mismatch# " << mismatch++ << ":: MCL Cluster: "<< i << " Size: " << mclClusters[i].size() << " HipMCL Cluster: "<< c2_max <<" Size: " << clusterSizes2[c2_max] << " last vertex in MCL output: " << mclClusters[i].back()<< endl;
+            
         }
         double Fi = 0;
         for(int j=0; j<isectCount; j++)
         {
             int64_t c2 = isect[j];
-            double precision = (double) nisect[c2] / clusterSizes2[j]; //
-            double recall = (double) nisect[c2] / clusters1[i].size();
+            double precision = (double) nisect[c2] / clusterSizes2[c2];
+            double recall = (double) nisect[c2] / mclClusters[i].size();
             double Fij = 2 * precision * recall / (precision + recall);
             Fi = max(Fi, Fij);
         }
         
-        Fi = Fi * clusters1[i].size()/ N1;
+        Fi = Fi * mclClusters[i].size()/ nproteins;
         F[i] = Fi;
     }
     return accumulate(F.begin(), F.end(), 0.0);
@@ -149,9 +149,9 @@ int main(int argc, char* argv[])
     int base = 0;
   
     
-    if(argc < 2)
+    if(argc != 7)
     {
-        cout << "Usage: ./fscore -M1 <FILENAME_Output_ MCL> -M2 <FILENAME_Output_HipMCL> -base <Base of indices 1 or 0>\n";
+        cout << "Usage: ./fscore -M1 <MCLOut> -M2 <HipMCLOut> -base <Base of vertices (same as HipMCL) 1 or 0>\n";
         cout << "Example: ./fscore -M1 input1.txt -M2 input2.txt -base 0 " << endl;
         return -1;
     }
@@ -161,12 +161,12 @@ int main(int argc, char* argv[])
         if (strcmp(argv[i],"-M1")==0)
         {
             ifilename1 = string(argv[i+1]);
-            printf("\nfilename1: %s",ifilename1.c_str());
+            printf("\nMCL output file: %s",ifilename1.c_str());
         }
         else if (strcmp(argv[i],"-M2")==0)
         {
             ifilename2 = string(argv[i+1]);
-            printf("\nfilename2: %s",ifilename2.c_str());
+            printf("\nHipMCL output file: %s",ifilename2.c_str());
         }
         else if (strcmp(argv[i],"-base")==0)
         {
