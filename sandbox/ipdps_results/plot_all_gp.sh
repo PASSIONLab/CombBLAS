b0ed5f2619d8019dc803282674228efefc20a34e
aydinbuluc <abuluc@lbl.gov>
2018-04-29 14:45:30 -0700

    removing more files

--- sandbox/ipdps_results/plot_all_gp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for f in *.gp
-do
-	gnuplot < $f
-done
--- sandbox/run-roofline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for proc in 1 4 9 16 25 36 49
-do
-	echo "============================== $proc procs: ==============================="
-	mpirun -np $proc python roofline_spmv_pure.py #> foo
-	#cat foo | python roofline_pure_parser.py
-done
