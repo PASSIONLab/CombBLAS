bfa53764ca4a69e42ff518f094bd9bbcfb7e7eef
nagasaka <nagasaka.mlab@gmail.com>
2018-10-17 00:33:11 -0700

    add HashSpGEMM to LocalSpGEMM

--- CombBLAS/include/CombBLAS/mtSpGEMM.h
+++ CombBLAS/include/CombBLAS/mtSpGEMM.h
@@ -70,6 +70,12 @@ T* prefixsum(T* in, int size, int nthreads)
 }
 
 
+template <typename IT, typename NT>
+bool sort_less(const std::pair<IT, NT> &left, const std::pair<IT, NT> &right)
+{
+    return left.first < right.first;
+}
+
 
 
 // multithreaded HeapSpGEMM
@@ -105,105 +111,216 @@ SpTuples<IT, NTO> * LocalSpGEMM
     }
 #endif
    
+    std::cout << "numThreads: " << numThreads << std::endl;
+
+    IT* flopC = estimateFLOP(A, B);
+    IT* flopptr = prefixsum<IT>(flopC, Bdcsc->nzc, numThreads);
+    IT flop = flopptr[Bdcsc->nzc];
+    std::cout << "FLOP of A * B is " << flop << std::endl;
+    delete [] flopC;
+
     IT* colnnzC = estimateNNZ(A, B);
     IT* colptrC = prefixsum<IT>(colnnzC, Bdcsc->nzc, numThreads);
     delete [] colnnzC;
     IT nnzc = colptrC[Bdcsc->nzc];
+    std::cout << "NNZ of A * B is " << nnzc << std::endl;
+
+    std::cout << "Compression ratio is " << (float)flop / nnzc << std::endl;
+
     std::tuple<IT,IT,NTO> * tuplesC = static_cast<std::tuple<IT,IT,NTO> *> (::operator new (sizeof(std::tuple<IT,IT,NTO>[nnzc])));
 	
-	
-
-	
-    // thread private space for heap and colinds
-    std::vector<std::vector< std::pair<IT,IT>>> colindsVec(numThreads);
-    std::vector<std::vector<HeapEntry<IT,NT1>>> globalheapVec(numThreads);
-    
-    for(int i=0; i<numThreads; i++) //inital allocation per thread, may be an overestimate, but does not require more memoty than inputs
+    if (1)  // Heap SpGEMM
     {
-        colindsVec[i].resize(nnzA/numThreads);
-        globalheapVec[i].resize(nnzA/numThreads);
-    }
+        // thread private space for heap and colinds
+        std::vector<std::vector< std::pair<IT,IT>>> colindsVec(numThreads);
+        std::vector<std::vector<HeapEntry<IT,NT1>>> globalheapVec(numThreads);
+    
+        for(int i=0; i<numThreads; i++) //inital allocation per thread, may be an overestimate, but does not require more memoty than inputs
+        {
+            colindsVec[i].resize(nnzA/numThreads);
+            globalheapVec[i].resize(nnzA/numThreads);
+        }
 
 
 #ifdef THREADED
 #pragma omp parallel for
 #endif
-    for(int i=0; i < Bdcsc->nzc; ++i)
-    {
-        size_t nnzcolB = Bdcsc->cp[i+1] - Bdcsc->cp[i]; //nnz in the current column of B
-		int myThread = 0;
-	#ifdef THREADED
-        myThread = omp_get_thread_num();
-	#endif
-        if(colindsVec[myThread].size() < nnzcolB) //resize thread private vectors if needed
+        for(int i=0; i < Bdcsc->nzc; ++i)
         {
-            colindsVec[myThread].resize(nnzcolB);
-            globalheapVec[myThread].resize(nnzcolB);
-        }
-        
-        
-        // colinds.first vector keeps indices to A.cp, i.e. it dereferences "colnums" vector (above),
-        // colinds.second vector keeps the end indices (i.e. it gives the index to the last valid element of A.cpnack)
-        Adcsc->FillColInds(Bdcsc->ir + Bdcsc->cp[i], nnzcolB, colindsVec[myThread], aux, csize);
-        std::pair<IT,IT> * colinds = colindsVec[myThread].data();
-        HeapEntry<IT,NT1> * wset = globalheapVec[myThread].data();
-        IT hsize = 0;
-        
-        
-        for(IT j = 0; (unsigned)j < nnzcolB; ++j)		// create the initial heap
-        {
-            if(colinds[j].first != colinds[j].second)	// current != end
+            size_t nnzcolB = Bdcsc->cp[i+1] - Bdcsc->cp[i]; //nnz in the current column of B
+            int myThread = 0;
+#ifdef THREADED
+            myThread = omp_get_thread_num();
+#endif
+            if(colindsVec[myThread].size() < nnzcolB) //resize thread private vectors if needed
             {
-                wset[hsize++] = HeapEntry< IT,NT1 > (Adcsc->ir[colinds[j].first], j, Adcsc->numx[colinds[j].first]);
+                colindsVec[myThread].resize(nnzcolB);
+                globalheapVec[myThread].resize(nnzcolB);
             }
-        }
-        std::make_heap(wset, wset+hsize);
         
-        IT curptr = colptrC[i];
-        while(hsize > 0)
-        {
-            std::pop_heap(wset, wset + hsize);         // result is stored in wset[hsize-1]
-            IT locb = wset[hsize-1].runr;	// relative location of the nonzero in B's current column
-            
-            NTO mrhs = SR::multiply(wset[hsize-1].num, Bdcsc->numx[Bdcsc->cp[i]+locb]);
-            if (!SR::returnedSAID())
+                
+            // colinds.first vector keeps indices to A.cp, i.e. it dereferences "colnums" vector (above),
+            // colinds.second vector keeps the end indices (i.e. it gives the index to the last valid element of A.cpnack)
+            Adcsc->FillColInds(Bdcsc->ir + Bdcsc->cp[i], nnzcolB, colindsVec[myThread], aux, csize);
+            std::pair<IT,IT> * colinds = colindsVec[myThread].data();
+            HeapEntry<IT,NT1> * wset = globalheapVec[myThread].data();
+            IT hsize = 0;
+        
+        
+            for(IT j = 0; (unsigned)j < nnzcolB; ++j)		// create the initial heap
             {
-                if( (curptr > colptrC[i]) && std::get<0>(tuplesC[curptr-1]) == wset[hsize-1].key)
+                if(colinds[j].first != colinds[j].second)	// current != end
                 {
-                    std::get<2>(tuplesC[curptr-1]) = SR::add(std::get<2>(tuplesC[curptr-1]), mrhs);
+                    wset[hsize++] = HeapEntry< IT,NT1 > (Adcsc->ir[colinds[j].first], j, Adcsc->numx[colinds[j].first]);
+                }
+            }
+            std::make_heap(wset, wset+hsize);
+        
+            IT curptr = colptrC[i];
+            while(hsize > 0)
+            {
+                std::pop_heap(wset, wset + hsize);         // result is stored in wset[hsize-1]
+                IT locb = wset[hsize-1].runr;	// relative location of the nonzero in B's current column
+            
+                NTO mrhs = SR::multiply(wset[hsize-1].num, Bdcsc->numx[Bdcsc->cp[i]+locb]);
+                if (!SR::returnedSAID())
+                {
+                    if( (curptr > colptrC[i]) && std::get<0>(tuplesC[curptr-1]) == wset[hsize-1].key)
+                    {
+                        std::get<2>(tuplesC[curptr-1]) = SR::add(std::get<2>(tuplesC[curptr-1]), mrhs);
+                    }
+                    else
+                    {
+                        tuplesC[curptr++]= std::make_tuple(wset[hsize-1].key, Bdcsc->jc[i], mrhs) ;
+                    }
+                }
+                if( (++(colinds[locb].first)) != colinds[locb].second)	// current != end
+                {
+                    // runr stays the same !
+                    wset[hsize-1].key = Adcsc->ir[colinds[locb].first];
+                    wset[hsize-1].num = Adcsc->numx[colinds[locb].first];
+                    std::push_heap(wset, wset+hsize);
                 }
                 else
                 {
-                    tuplesC[curptr++]= std::make_tuple(wset[hsize-1].key, Bdcsc->jc[i], mrhs) ;
+                    --hsize;
                 }
-                
-            }
-            
-            if( (++(colinds[locb].first)) != colinds[locb].second)	// current != end
-            {
-                // runr stays the same !
-                wset[hsize-1].key = Adcsc->ir[colinds[locb].first];
-                wset[hsize-1].num = Adcsc->numx[colinds[locb].first];
-                std::push_heap(wset, wset+hsize);
-            }
-            else
-            {
-                --hsize;
             }
         }
     }
     
+    else // Hash SpGEMM
+    {
+        const IT minHashTableSize = 16;
+        const IT hashScale = 107;
+
+        // thread private space for hash tables and colinds
+        std::vector<std::vector< std::pair<IT,IT>>> colindsVec(numThreads);
+        std::vector<std::vector< std::pair<IT,NT1>>> globalHashVec(numThreads);
+
+        for(int i=0; i<numThreads; i++) //inital allocation per thread, for colindsVec, may be an overestimate, but does not require more memoty than inputs. For globalHashVec, this is just minimum.
+        {
+            colindsVec[i].resize(nnzA/numThreads);
+            globalHashVec[i].resize(minHashTableSize);
+        }
+
+#ifdef THREADED
+#pragma omp parallel for
+#endif
+        for(IT i=0; i < Bdcsc->nzc; ++i)
+        {
+            int myThread = 0;
+#ifdef THREADED
+            myThread = omp_get_thread_num();
+#endif
+            size_t nnzcolB = Bdcsc->cp[i+1] - Bdcsc->cp[i]; //nnz in the current column of B
+            size_t nnzcolC = colptrC[i+1] - colptrC[i]; //nnz in the current column of C (=Output)
+            if(colindsVec[myThread].size() < nnzcolB) //resize thread private vectors if needed
+            {
+                colindsVec[myThread].resize(nnzcolB);
+            }
+
+            IT ht_size = minHashTableSize;
+            while(ht_size < nnzcolC) //ht_size is set as 2^n
+            {
+                ht_size <<= 1;
+            }
+            if(globalHashVec[myThread].size() < ht_size)
+            {
+                globalHashVec[myThread].resize(ht_size);
+            }
+
+            // colinds.first vector keeps indices to A.cp, i.e. it dereferences "colnums" vector (above),
+            // colinds.second vector keeps the end indices (i.e. it gives the index to the last valid element of A.cpnack)
+            Adcsc->FillColInds(Bdcsc->ir + Bdcsc->cp[i], nnzcolB, colindsVec[myThread], aux, csize);
+            std::pair<IT,IT> * colinds = colindsVec[myThread].data();
+            
+            // Initialize hash tables
+            for(IT j=0; (unsigned)j < ht_size; ++j)
+            {
+                globalHashVec[myThread][j].first = -1;
+            }
+            
+            // Multiply and add on Hash table
+            for (IT j=0; (unsigned)j < nnzcolB; ++j)
+            {
+                IT t_bcol = Bdcsc->ir[Bdcsc->cp[i] + j];
+                NT2 t_bval = Bdcsc->numx[Bdcsc->cp[i] + j];
+                for (IT k = colinds[j].first; (unsigned)k < colinds[j].second; ++k)
+                {
+                    NTO mrhs = SR::multiply(Adcsc->numx[k], t_bval);
+                    IT key = Adcsc->ir[k];
+                    IT hash = (key*hashScale) & (ht_size-1);
+                    while (1) //hash probing
+                    {
+                        if (globalHashVec[myThread][hash].first == key) //key is found in hash table
+                        {
+                            globalHashVec[myThread][hash].second = SR::add(mrhs, globalHashVec[myThread][hash].second);
+                            break;
+                        }
+                        else if (globalHashVec[myThread][hash].first == -1) //key is not registered yet
+                        {
+                            globalHashVec[myThread][hash].first = key;
+                            globalHashVec[myThread][hash].second = mrhs;
+                            break;
+                        }
+                        else //key is not found
+                        {
+                            hash = (hash+1) & (ht_size-1);
+                        }
+                    }
+                }
+            }
+            // gather non-zero elements from hash table, and then sort them by row indices
+            IT index = 0;
+            for (IT j=0; j < ht_size; ++j)
+            {
+                if (globalHashVec[myThread][j].first != -1)
+                {
+                    globalHashVec[myThread][index++] = globalHashVec[myThread][j];
+                }
+            }
+            std::sort(globalHashVec[myThread].begin(), globalHashVec[myThread].begin() + index, sort_less<IT, NT1>);
+
+            IT curptr = colptrC[i];
+            for (IT j=0; j < index; ++j)
+            {
+                tuplesC[curptr++]= std::make_tuple(globalHashVec[myThread][j].first, Bdcsc->jc[i], globalHashVec[myThread][j].second);
+            }
+        }
+    }
+
     if(clearA)
         delete const_cast<SpDCCols<IT, NT1> *>(&A);
     if(clearB)
         delete const_cast<SpDCCols<IT, NT2> *>(&B);
     
     delete [] colptrC;
+    delete [] flopptr;
     delete [] aux;
     
     SpTuples<IT, NTO>* spTuplesC = new SpTuples<IT, NTO> (nnzc, mdim, ndim, tuplesC, true, true);
     return spTuplesC;
-    
 }
 
 // estimate space for result of SpGEMM
@@ -315,6 +432,83 @@ IT* estimateNNZ(const SpDCCols<IT, NT1> & A,const SpDCCols<IT, NT2> & B)
     
     delete [] aux;
     return colnnzC;
+}
+
+// estimate space for result of SpGEMM
+template <typename IT, typename NT1, typename NT2>
+IT* estimateFLOP(const SpDCCols<IT, NT1> & A,const SpDCCols<IT, NT2> & B)
+{
+    IT nnzA = A.getnnz();
+    if(A.isZero() || B.isZero())
+    {
+        return NULL;
+    }
+    
+    Dcsc<IT,NT1>* Adcsc = A.GetDCSC();
+    Dcsc<IT,NT2>* Bdcsc = B.GetDCSC();
+    
+    float cf  = static_cast<float>(A.getncol()+1) / static_cast<float>(Adcsc->nzc);
+    IT csize = static_cast<IT>(ceil(cf));   // chunk size
+    IT * aux;
+    Adcsc->ConstructAux(A.getncol(), aux);
+	
+	
+    int numThreads = 1;
+#ifdef THREADED
+#pragma omp parallel
+    {
+        numThreads = omp_get_num_threads();
+    }
+#endif
+    
+
+    IT* colflopC = new IT[Bdcsc->nzc]; // flop in every nonempty column of C
+	
+#ifdef THREADED
+#pragma omp parallel for
+#endif
+    for(IT i=0; i< Bdcsc->nzc; ++i)
+    {
+        colflopC[i] = 0;
+    }
+    
+    
+
+
+    // thread private space for heap and colinds
+    std::vector<std::vector< std::pair<IT,IT>>> colindsVec(numThreads);
+
+	
+    for(int i=0; i<numThreads; i++) //inital allocation per thread, may be an overestimate, but does not require more memoty than inputs
+    {
+        colindsVec[i].resize(nnzA/numThreads);
+    }
+
+#ifdef THREADED
+#pragma omp parallel for
+#endif
+    for(int i=0; i < Bdcsc->nzc; ++i)
+    {
+        size_t nnzcolB = Bdcsc->cp[i+1] - Bdcsc->cp[i]; //nnz in the current column of B
+		int myThread = 0;
+#ifdef THREADED
+        myThread = omp_get_thread_num();
+#endif
+        if(colindsVec[myThread].size() < nnzcolB) //resize thread private vectors if needed
+        {
+            colindsVec[myThread].resize(nnzcolB);
+        }
+		
+        // colinds.first vector keeps indices to A.cp, i.e. it dereferences "colnums" vector (above),
+        // colinds.second vector keeps the end indices (i.e. it gives the index to the last valid element of A.cpnack)
+        Adcsc->FillColInds(Bdcsc->ir + Bdcsc->cp[i], nnzcolB, colindsVec[myThread], aux, csize);
+        for (IT j = 0; (unsigned)j < nnzcolB; ++j) {
+            colflopC[i] += colindsVec[myThread][j].second - colindsVec[myThread][j].first;
+        }
+    }
+    
+    delete [] aux;
+    return colflopC;
 }
 
 }
