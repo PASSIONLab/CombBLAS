60fc44113a6343432fca4076b3feb9ba9db36441
Taufique Hussain <mth@indiana.edu>
2019-03-26 21:15:13 -0400

    Added details of execution flow of owner function of SpParMat3D

--- CombBLAS/include/CombBLAS/SpParMat3D.cpp
+++ CombBLAS/include/CombBLAS/SpParMat3D.cpp
@@ -56,49 +56,75 @@ namespace combblas
         // Get total number of processors in the original 2D CommGrid.
         // Because number of processors in each layer of 3D grid would be determined from this number
         int nprocs = commGrid2D->GetSize();
-        cout << "nprocs " << nprocs << endl;
         // Create a 3D CommGrid with all the processors involved in the 2D CommGrid
         commGrid3D.reset(new CommGrid3D(commGrid2D->GetWorld(), nlayers, 0, 0));
-        cout << "CommGrid3D formed" << endl;
         
+        // Total number of rows in the matrix
         IT nrows = A2D.getnrow();
+        // Total number of cols in the matrix
         IT ncols = A2D.getncol();
+        // Number of rows in original 2D processor grid in which matrix is distributed
         int pr2d = commGrid2D->GetGridRows();
+        // Number of cols in original 2D processor grid in which matrix is distributed
         int pc2d = commGrid2D->GetGridCols();
+        // On which row in original proc grid does this processor belong
         int rowrank2d = commGrid2D->GetRankInProcRow();
+        // On which column in original proc grid does this processor belong
         int colrank2d = commGrid2D->GetRankInProcCol();
+        // How many rows of global matrix does each processor in 2D proc grid contain
         IT m_perproc2d = nrows / pr2d;
+        // How many columns of global matrix does each processor in 2D proc grid contain
         IT n_perproc2d = ncols / pc2d;
         DER* spSeq = A2D.seqptr(); // local submatrix
+        // Global row index of matrix from where current processor starts to store
         IT localRowStart2d = colrank2d * m_perproc2d; // first row in this process
+        // Global column index of matrix from where current processor starts to store
         IT localColStart2d = rowrank2d * n_perproc2d; // first col in this process
-        
+        cout << localRowStart2d << " , " << localColStart2d << endl;
 
         LIT lrow3d, lcol3d;
+        // Data structure to contain the information about how much data would be sent to each other processors from current processor.
         std::vector<int> tsendcnt(nprocs,0);
+        // Iterate over all local columns in current processor
         for(typename DER::SpColIter colit = spSeq->begcol(); colit != spSeq->endcol(); ++colit)
         {
+            // Calculate global column index of corresponding local column index
             IT gcol = colit.colid() + localColStart2d;
+            // Iterate over all local nonzero entries in current column
             for(typename DER::SpColIter::NzIter nzit = spSeq->begnz(colit); nzit != spSeq->endnz(colit); ++nzit)
             {
+                // Calculate global row index for corresponding nonzero
                 IT grow = nzit.rowid() + localRowStart2d;
                 
                 //, nzit.value();
+                // Calcualting owner of the particular nonzero in 3D processor grid
+                // As well as what will be the row and column index of the non-zero after going to 3D
                 int owner = Owner(nrows, ncols, grow, gcol, lrow3d, lcol3d); //3D owner
+                // Incrementing number of data to send to the calculated owner
                 tsendcnt[owner]++;
             }
         }
         
+        // Calculation of how much data would be sent to which processor is done
+        // Now those data need to be prepared to be sent
+        // Initializing a dutu structure which would hold the data to send
         std::vector<std::vector<std::tuple<IT,IT, NT>>> sendTuples (nprocs);
+        // Iterate over all local columns in current processor
         for(typename DER::SpColIter colit = spSeq->begcol(); colit != spSeq->endcol(); ++colit)
         {
+            // Calculate global column index of corresponding local column index
             IT gcol = colit.colid() + localColStart2d;
+            // Iterate over all local nonzero entries in current column
             for(typename DER::SpColIter::NzIter nzit = spSeq->begnz(colit); nzit != spSeq->endnz(colit); ++nzit)
             {
+                // Calculate global row index for corresponding nonzero
                 IT grow = nzit.rowid() + localRowStart2d;
-                
+                // Getting value of the non-zero 
                 NT val = nzit.value();
+                // Calcualting owner of the particular nonzero in 3D processor grid
+                // As well as what will be the row and column index of the non-zero after going to 3D
                 int owner = Owner(nrows, ncols, grow, gcol, lrow3d, lcol3d); //3D owner
+                // Creating a tuple for each non-zero entry and pushing it to appropriate place of the data structure
                 sendTuples[owner].push_back(std::make_tuple(lrow3d, lcol3d, val));
             }
         }
@@ -114,68 +140,102 @@ namespace combblas
         std::shared_ptr<CommGrid> commGridLayer = commGrid3D->commGridLayer;
         layermat = new SpParMat<IT, NT, DER>(localm3d, commGridLayer);
     }
-    
+   
+    // Function to calculate owner processor of a particular non-zero in 3D processor grid
+    // Patameters:
+    //      - total number of vertices in whole matrix
+    //      - total number of columns in whole matrix
+    //      - global row index of the non-zero
+    //      - global column index of the non-zero
+    //      - reference to a variable which holds local row index of matrix in the 3D processor grid
+    //      - reference to a variable which holds local column index of matrix in the 3D processor grid
     template <class IT, class NT,class DER>
     template <typename LIT>
     int SpParMat3D<IT,NT,DER>::Owner(IT total_m, IT total_n, IT grow, IT gcol, LIT & lrow, LIT & lcol) const
     {
         
         // first map to Layer 0 and then split
-        std::shared_ptr<CommGrid> commGridLayer = commGrid3D->commGridLayer; // CommGrid for my layer
+        // We would consider distributing whole matrix on only one layer(let say layer zero or L0) of 3D CommGrid. Then split accordingly.
+        std::shared_ptr<CommGrid> commGridLayer = commGrid3D->commGridLayer; // 2D CommGrid for my layer
+        // Getting how many processor rows in this layer
         int procrows = commGridLayer->GetGridRows();
+        // Getting how many processor columns in this layer
         int proccols = commGridLayer->GetGridCols();
+        // Getting how many layer in the 3D CommGrid
         int nlayers = commGrid3D->gridLayers;
         
+        // If distributed in this way
+        // Except the processors in last row of layer, how many rows each processors are supposed to contain
         IT m_perproc_L0 = total_m / procrows;
+        // Except the processors in last column of layer, how many columns each processors are supposed to contain
         IT n_perproc_L0 = total_n / proccols;
         
         int procrow_L0; // within a layer
-        if(m_perproc_L0 != 0)
+        // If in average per process contains at least one row
+        if(m_perproc_L0 != 0) // If on average per processor contains at least one row
         {
+            // Calculating on which row of layer zero would this non-zero belong
             procrow_L0 = std::min(static_cast<int>(grow / m_perproc_L0), procrows-1);
         }
         else    // all owned by the last processor row
         {
             procrow_L0 = procrows -1;
         }
-        int proccol_L0;
+        int proccol_L0; // within a layer
+        // If in average per process contains at least one column
         if(n_perproc_L0 != 0)
         {
+            // Calculating on which column of layer zero would this non-zero belong
             proccol_L0 = std::min(static_cast<int>(gcol / n_perproc_L0), proccols-1);
         }
-        else
+        else 
         {
             proccol_L0 = proccols-1;
         }
         
+        // Now calculating what will be local row and column index of this non-zero if 
+        // it belongs to the processor of just calculated row and column in layer zero
         IT lrow_L0 = grow - (procrow_L0 * m_perproc_L0);
         IT lcol_L0 = gcol - (proccol_L0 * n_perproc_L0);
+        // Now if we split content of each processor in layer zero and scatter along layer,
+        // processor row and column of that non-zero would be same independent of which layer it is going
+        int proccol_layer = proccol_L0;
+        int procrow_layer = procrow_L0;
         int layer;
         // next, split and scatter
-        if(colsplit)
+        if(colsplit) // If local matrices are split along column to go to 3D
         {
+            // Previously we calculated number of columns belonging to per processor in layer zero
+            // Now we need to calculate number of columns belonging to per processor along layer after splitting and scattering
             IT n_perproc;
             if(proccol_L0 < commGrid3D->gridCols-1)
             {
+                // If this non-zero doesn't belong to last processor, number of columns contained by the processor is the average we calcualted
+                // So we are using that to calcualte for per processor along layer
                 n_perproc = n_perproc_L0 / nlayers;
             }
             else
             {
+                // If this non-zero doesn't belong to last processor, number of columns contained by the processor may not be the average we calcualted
+                // So we are taking count of rest of the columns to calcualte for per processor along layer
                 n_perproc = (total_n - (n_perproc_L0 * proccol_L0)) / nlayers;
             }
+            // If in average per processor along layer contain more than zero columns
             if(n_perproc != 0)
             {
+                // Calculating in which layer non-zero should belong
                 layer = std::min(static_cast<int>(lcol_L0 / n_perproc), nlayers-1);
             }
             else
                 layer = nlayers-1;
             
+            // If splitting along column then local row index of that non-zero in 3D would be same as it is now
             lrow = lrow_L0;
+            // But local column index would change
             lcol = lcol_L0 - (layer * n_perproc);
         }
-        else
+        else // If local matrices are split along row to go to 3D. Logics would be similar to column split
         {
-            
             IT m_perproc;
             if(procrow_L0 < commGrid3D->gridRows-1)
             {
@@ -195,8 +255,8 @@ namespace combblas
             lcol = lcol_L0;
             lrow = lrow_L0 - (layer * m_perproc);
         }
-        int proccol_layer = proccol_L0;
-        int procrow_layer = procrow_L0;
+        // Now we know in which row and column of which layer would this non-zero go if translated to 3D
+        // ID of that processor would be the owner ID, so that would be returned
         return commGrid3D->GetRank(layer, procrow_layer, proccol_layer);
     }
     
