444bf0daa2d2ffe88b774f0fb32d558e6e5e95db
Taufique Hussain <mth@indiana.edu>
2019-04-19 12:14:54 -0400

    Changed recvTuples from vector of tuples to pointer of tuples

--- CombBLAS/include/CombBLAS/SpParMat3D.cpp
+++ CombBLAS/include/CombBLAS/SpParMat3D.cpp
@@ -497,49 +497,58 @@ namespace combblas
 
         std::vector< std::tuple<IT,IT,NT> > sendTuples;
         for (int i = 0; i < numChunks; i++){
-            if(myrank == 12 && i == 1){
-                printf("nnz: %d, mdim: %d, ndim: %d\n",sendprfl[i*3], sendprfl[i*3+1], sendprfl[i*3+2]);
-            }
-            int cnt = 0;
             for(typename DER::SpColIter colit = localChunks[i].begcol(); colit != localChunks[i].endcol(); ++colit){
                 for(typename DER::SpColIter::NzIter nzit = localChunks[i].begnz(colit); nzit != localChunks[i].endnz(colit); ++nzit){
-                    cnt++;
                     NT val = nzit.value();
-                    if(myrank == 12 && i == 1 && (cnt == 1 || cnt == sendcnt[i])){
-                        cout << nzit.rowid() << " " << colit.colid() << " " << nzit.value() << endl;
-                    }
                     sendTuples.push_back(std::make_tuple(nzit.rowid(), colit.colid(), nzit.value()));
                 }
             }
         }
-        //for(int i = 0; i < totsend; i++){
-            //cout << get<0>(sendTuples[i]) << " " << get<1>(sendTuples[i]) << " " << get<2>(sendTuples[i]) << endl;
-        //}
-        //std::tuple<IT,IT,NT>* recvTuples = new std::tuple<IT,IT,NT>[totrecv];
-        std::vector< std::tuple<IT,IT,NT> > recvTuples(totrecv);
-        MPI_Alltoallv(sendTuples.data(), sendcnt, sdispls, MPI_tuple, recvTuples.data(), recvcnt, rdispls, MPI_tuple, World);
-        //for(int i = 0; i < totrecv; i++){
-            //cout << get<0>(recvTuples[i]) << " " << get<1>(recvTuples[i]) << " " << get<2>(recvTuples[i]) << endl;
-        //}
-        vector< vector< tuple<IT, IT, NT> > > tempTuples(numChunks);
-        for (int i = 0; i < numChunks; i++){
-            for (int j = rdispls[i]; j < rdispls[i]+recvcnt[i]; j++){
-                tempTuples[i].push_back(recvTuples[j]);
-            }
-            if(myrank == 6 && i == 2){
-                printf("nnz: %d, mdim: %d, ndim: %d\n",recvprfl[i*3], recvprfl[i*3+1], recvprfl[i*3+2]);
-                cout << get<0>(tempTuples[i][0]) << " " << get<1>(tempTuples[i][0]) << " " << get<2>(tempTuples[i][0]) << endl;
-                cout << get<0>(tempTuples[i][tempTuples[i].size()-1]) << " " << get<1>(tempTuples[i][tempTuples[i].size()-1]) << " " << get<2>(tempTuples[i][tempTuples[i].size()-1]) << endl;
-            }
+        if(myrank == 12){
+            int i = 1;
+            printf("[SENDING] Processor: %d, Chunk: %d\n", myrank, i);
+            printf("nnz: %d, mdim: %d, ndim: %d\n",sendprfl[i*3], sendprfl[i*3+1], sendprfl[i*3+2]);
+            printf("first tuple: < %lld, %lld, %lf >\n", 
+                    get<0>(sendTuples[sdispls[i]]), 
+                    get<1>(sendTuples[sdispls[i]]), 
+                    get<2>(sendTuples[sdispls[i]]));
+            printf("last tuple: < %lld, %lld, %lf >\n", 
+                    get<0>(sendTuples[sdispls[i]+sendcnt[i]-1]), 
+                    get<1>(sendTuples[sdispls[i]+sendcnt[i]-1]), 
+                    get<2>(sendTuples[sdispls[i]+sendcnt[i]-1]));
+            //for(int j = sdispls[i]; j < sdispls[i]+sendcnt[i]; j++){
+                //cout << get<0>(sendTuples[j]) << " " << get<1>(sendTuples[j]) << " " << get<2>(sendTuples[j]) << endl;
+            //}
         }
+        std::tuple<IT,IT,NT>* recvTuples = new std::tuple<IT,IT,NT>[totrecv];
+        MPI_Alltoallv(sendTuples.data(), sendcnt, sdispls, MPI_tuple, recvTuples, recvcnt, rdispls, MPI_tuple, World);
+        //std::vector< std::tuple<IT,IT,NT> > recvTuples(totrecv);
+        //MPI_Alltoallv(sendTuples.data(), sendcnt, sdispls, MPI_tuple, recvTuples.data(), recvcnt, rdispls, MPI_tuple, World);
         if(myrank == 6){
             int i = 2;
-            //SpTuples<IT,NT>xt(tempTuples[i].size(), recvprfl[i*3+1], recvprfl[i*3+2], tempTuples[i].data(), true, true);
-            vector < tuple < IT, IT, NT > > dumv;
-            dumv.push_back(make_tuple(0,0,1));
-            dumv.push_back(make_tuple(1,1,1));
-            SpTuples<IT,NT>xt(dumv.size(), 2, 2, dumv.data());
-            //DER * x = new DER(SpTuples<IT,NT>(tempTuples[i].size(), recvprfl[i*3+1], recvprfl[i*3+2], tempTuples[i].data()), false);
+            printf("[RECEIVING] Processor: %d, Chunk: %d\n", myrank, i);
+            printf("nnz: %d, mdim: %d, ndim: %d\n",recvprfl[i*3], recvprfl[i*3+1], recvprfl[i*3+2]);
+            printf("first tuple: < %lld, %lld, %lf >\n", 
+                    get<0>(recvTuples[rdispls[i]]), 
+                    get<1>(recvTuples[rdispls[i]]), 
+                    get<2>(recvTuples[rdispls[i]]));
+            printf("last tuple: < %lld, %lld, %lf >\n", 
+                    get<0>(recvTuples[rdispls[i]+recvcnt[i]-1]), 
+                    get<1>(recvTuples[rdispls[i]+recvcnt[i]-1]), 
+                    get<2>(recvTuples[rdispls[i]+recvcnt[i]-1]));
+            //for(int j = rdispls[i]; j < rdispls[i]+recvcnt[i]; j++){
+                //cout << get<0>(recvTuples[j]) << " " << get<1>(recvTuples[j]) << " " << get<2>(recvTuples[j]) << endl;
+            //}
+            
+            SpTuples<IT,NT>xt(recvprfl[i*3], recvprfl[i*3+1], recvprfl[i*3+2], recvTuples+rdispls[i]);
+            //vector < tuple < IT, IT, NT > > dumv;
+            //dumv.push_back(make_tuple(0,0,1));
+            //dumv.push_back(make_tuple(1,1,1));
+            //SpTuples<IT,NT>xt(dumv.size(), 2, 2, dumv.data());
+            //tuple<IT, IT, NT> * dumv = new tuple<IT,IT,NT>[2];
+            //dumv[0] = make_tuple(0,0,1);
+            //dumv[1] = make_tuple(1,1,1);
+            //SpTuples<IT,NT>xt(2, 2, 2, dumv);
         }
 
         ////IT mdim, ndim;
