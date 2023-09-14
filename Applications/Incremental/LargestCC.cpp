5e2b8e7e4e10f7711c4ad93b2aa3d04c9966ffee
Taufique Hussain <mth@indiana.edu>
2023-09-14 12:38:22 -0700

    Script for dumping largest connected component updated

--- Applications/Incremental/LargestCC.cpp
+++ Applications/Incremental/LargestCC.cpp
@@ -70,40 +70,43 @@ int main(int argc, char* argv[])
     {
         if(myrank == 0)
         {
-            cout << "Usage: ./cc -I <mm|triples> -M <FILENAME_MATRIX_MARKET> (required)\n";
-            cout << "-I <INPUT FILE TYPE> (mm: matrix market, triples: (vtx1, vtx2, edge_weight) triples. default:mm)\n";
-            cout << "-base <BASE OF MATRIX MARKET> (default:1)\n";
-            cout << "-rand <RANDOMLY PERMUTE VERTICES> (default:0)\n";
-            cout << "Example (0-indexed mtx with random permutation): ./cc -M input.mtx -base 0 -rand 1" << endl;
-            cout << "Example (triples format): ./cc -I triples -M input.txt" << endl;
+            cout << "Usage: ./cc --infile-type <mm|triples> --infile <FILENAME_MATRIX_MARKET> --outfile <FILENAME_MATRIX_MARKET> (required)\n";
+            cout << "--infile-type <INPUT FILE TYPE> (mm: matrix market, triples: (vtx1, vtx2, edge_weight) triples. default:mm)\n";
+            cout << "--base <BASE OF MATRIX MARKET> (default:1)\n";
         }
         MPI_Finalize();
         return -1;
     }
     {
         string ifilename = "";
+        string ofilename = "";
         int base = 1;
         int randpermute = 1;
         bool isMatrixMarket = true;
         
         for (int i = 1; i < argc; i++)
         {
-            if (strcmp(argv[i],"-I")==0)
+            if (strcmp(argv[i],"--infile-type")==0)
             {
                 string ifiletype = string(argv[i+1]);
                 if(ifiletype == "triples") isMatrixMarket = false;
             }
-            if (strcmp(argv[i],"-M")==0)
+            else if (strcmp(argv[i],"--infile")==0)
             {
                 ifilename = string(argv[i+1]);
-                if(myrank == 0) printf("filename: %s\n",ifilename.c_str());
+                if(myrank == 0) printf("Input filename: %s\n",ifilename.c_str());
             }
-            else if (strcmp(argv[i],"-base")==0)
+            else if (strcmp(argv[i],"--outfile")==0)
+            {
+                ofilename = string(argv[i+1]);
+                if(myrank == 0) printf("Output filename: %s\n",ofilename.c_str());
+            }
+            else if (strcmp(argv[i],"--base")==0)
             {
                 base = atoi(argv[i + 1]);
                 if(myrank == 0) printf("\nBase of MM (1 or 0):%d",base);
             }
-            else if (strcmp(argv[i],"-rand")==0)
+            else if (strcmp(argv[i],"--rand")==0)
             {
                 randpermute = atoi(argv[i + 1]);
                 if(myrank == 0) printf("\nRandomly permute the matrix? (1 or 0):%d",randpermute);
@@ -161,13 +164,13 @@ int main(int argc, char* argv[])
         FullyDistVec<int64_t, int64_t> ccLabels = CC(A, nCC);
         
         double t2 = MPI_Wtime();
-        string outname = ifilename + ".components";
-        ccLabels.ParallelWrite(outname, base);
+        //string outname = ifilename + ".components";
+        //ccLabels.ParallelWrite(outname, base);
         int64_t nclusters = ccLabels.Reduce(maximum<int64_t>(), (int64_t) 0 ) ;
         nclusters ++; // because of zero based indexing for clusters
 
         stringstream s2;
-        s2 << "Number of clusters: " << nclusters << endl;
+        s2 << "Number of connected components: " << nclusters << endl;
         s2 << "Total time: " << (t2-t1) << endl;
         s2 <<  "=================================================\n" << endl ;
         SpParHelper::Print(s2.str());
@@ -211,7 +214,7 @@ int main(int argc, char* argv[])
 		FullyDistVec<int64_t,int64_t> isov = ccLabels.FindInds(bind2nd(equal_to<int64_t>(), largestCC));
 		Dist::MPI_DCCols MCC = M.SubsRef_SR<PTNTBOOL,PTBOOLNT>(isov, isov, false);
         MCC.PrintInfo();
-        MCC.ParallelWriteMM(ifilename+std::string(".lcc"), true);
+        MCC.ParallelWriteMM(ofilename, true);
     }
     
     MPI_Finalize();
--- Applications/Incremental/lcc.sh
+++ Applications/Incremental/lcc.sh
@@ -1,26 +1,37 @@
 #!/bin/bash -l
 
-#SBATCH -q debug
-#SBATCH -C knl
+#SBATCH -q debug 
+#SBATCH -C cpu
 ##SBATCH -A m1982 # Aydin's project (PASSION: Scalable Graph Learning for Scientific Discovery)
 ##SBATCH -A m2865 # Exabiome project
-#SBATCH -A m4012 # Ariful's project (GraphML: Intelligent Premitives for Graph Machine Learning)
+##SBATCH -A m4012 # Ariful's project (GraphML: Intelligent Premitives for Graph Machine Learning)
+#SBATCH -A m4293 # Sparsitute project (A Mathematical Institute for Sparse Computations in Science and Engineering)
 
-#SBATCH -t 00:30:00
+#SBATCH -t 0:30:00
 
-#SBATCH -N 256
+#SBATCH -N 4
 #SBATCH -J lcc
-#SBATCH -o lcc.o%j
+#SBATCH -o slurm.lcc.o%j
 
-export OMP_NUM_THREADS=4
+N_NODE=4
+PROC_PER_NODE=16
+N_PROC=$(( $N_NODE * $PROC_PER_NODE ))
+THREAD_PER_PROC=8
+export OMP_NUM_THREADS=$THREAD_PER_PROC
 
-LCC_BIN=/global/homes/t/taufique/Codes/CombBLAS/_build/Applications/Incremental/lcc
+BINARY=$HOME/Codes/CombBLAS/_build/Applications/Incremental/lcc
 
-#MFILE=/global/cscratch1/sd/taufique/vir_vs_vir_30_50length_propermm.mtx
-#srun -N 1 -n 16 -c 4 --ntasks-per-node=16 --cpu-bind=cores $PREP_SPLIT_BIN -I mm -M $MFILE -N 20
+#DATA_NAME=virus
+#IN_FILE=$CFS/m1982/HipMCL/viruses/Renamed_vir_vs_vir_30_50length.indexed.mtx
+#OUT_FILE=$CFS/m1982/taufique/virus-lcc/lcc_Renamed_vir_vs_vir_30_50length.indexed.mtx
 
-MFILE=/global/cscratch1/sd/taufique/Renamed_euk_vs_euk_30_50length.indexed.mtx
-srun -N 256 -n 4096 -c 4 --ntasks-per-node=16 --cpu-bind=cores $LCC_BIN -I mm -M $MFILE
+DATA_NAME=eukarya
+IN_FILE=$CFS/m1982/HipMCL/eukarya/Renamed_euk_vs_euk_30_50length.indexed.mtx
+OUT_FILE=$CFS/m1982/taufique/eukarya-lcc/lcc_Renamed_euk_vs_euk_30_50length.indexed.mtx
 
-#MFILE=/global/cscratch1/sd/taufique/Renamed_arch_vs_arch_30_50length.indexed.mtx
-#srun -N 4 -n 64 -c 4 --ntasks-per-node=16 --cpu-bind=cores $LCC_BIN -I mm -M $MFILE
+#DATA_NAME=isolates-subgraph1
+#IN_FILE=$CFS/m1982/HipMCL/iso_m100/subgraph1/subgraph1_iso_vs_iso_30_70length_ALL.m100.indexed.mtx
+#OUT_FILE=$CFS/m1982/taufique/isolates-subgraph1-lcc/lcc_subgraph1_iso_vs_iso_30_70length_ALL.m100.indexed.mtx
+
+srun -N $N_NODE -n $N_PROC -c $THREAD_PER_PROC --ntasks-per-node=$PROC_PER_NODE --cpu-bind=cores \
+    $BINARY --infile $IN_FILE --infile-type mm --outfile $OUT_FILE
